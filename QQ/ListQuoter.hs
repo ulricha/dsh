2d5f9da7eab122a2a1f6582be0c09b8fcd2855ce
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-06-15 15:59:04 +0200

    Parser ready

--- QQ/ListQuoter.hs
+++ QQ/ListQuoter.hs
@@ -6,6 +6,7 @@ import Language.Haskell.TH.Quote
 import Language.Haskell.Exts.Parser
 import Language.Haskell.Exts.Syntax
 import Language.Haskell.SyntaxTrees.ExtsToTH
+-- import Language.Haskell.Exts.InternalParser
 
 quoteListCompr :: String -> TH.ExpQ
 quoteListCompr = transform . parseCompr
@@ -13,13 +14,13 @@ quoteListCompr = transform . parseCompr
 quoteListComprPat :: String -> TH.PatQ
 quoteListComprPat = undefined
 
-transform e = let r = translateExtsToTH e
-	       in case r of
-	           Left e -> error "Jikes"
-		   Right e -> return e
+transform e = let r = show e
+	       in TH.litE $ TH.stringL r
 
 parseCompr :: String -> Exp
-parseCompr = fromParseResult . parseExp
+parseCompr = fromParseResult . parseExp . expand
 
+expand :: String -> String
+expand e = '[':(e ++ "]")
 ferryHaskell :: QuasiQuoter
 ferryHaskell = QuasiQuoter quoteListCompr quoteListComprPat 
