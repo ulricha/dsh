3a26b8b5d19afeea6445b7be93314a0562530435
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-08-19 14:48:58 +0200

    Added Haskell version of example from paper

---
+++ examples/PaperExample.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE ViewPatterns, QuasiQuotes, TemplateHaskell, TransformListComp #-}
+module PaperExample where
+    
+import Ferry (toQ, fromQ, Q (..), view, qc)
+import qualified Ferry as Q
+import Database.HDBC.Sqlite3
+import GHC.Exts (the)
+import Data.List (nub)
+
+conn :: Connection
+conn = undefined
+
+type Facility = String
+type Cat      = String
+type Feature  = String
+type Meaning  = String
+
+facilities :: [(Facility, Cat)]
+facilities = [ ("SQL", "QLA"),
+               ("ODBC", "API"),
+               ("LINQ", "LIN"),
+               ("Links", "LIN"),
+               ("Rails", "ORM"),
+               ("Ferry", "LIB"),
+               ("Kleisli", "QLA"),
+               ("ADO.NET", "ORM"),
+               ("HaskellDB", "LIB")]
+               
+features :: [(Facility, Feature)]
+features = [("Kleisli","nest")
+          ,("Kleisli","comp")
+          ,("Kleisli","type")
+          ,("Links","comp")
+          ,("Links","type")
+          ,("Links","SQL")
+          ,("LINQ","nest")
+          ,("LINQ","comp")
+          ,("LINQ","type")
+          ,("HaskellDB","comp")
+          ,("HaskellDB","type")
+          ,("HaskellDB","SQL")
+          ,("SQL","aval")
+          ,("SQL","type")
+          ,("SQL","SQL")
+          ,("Rails","nest")
+          ,("Rails","maps")
+          ,("ADO.NET","maps")
+          ,("ADO.NET","comp")
+          ,("ADO.NET","type")
+          ,("Ferry","list")
+          ,("Ferry","nest")
+          ,("Ferry","comp")
+          ,("Ferry","aval")
+          ,("Ferry","type")
+          ,("Ferry","SQL")] 
+          
+meanings :: [(Feature, Meaning)]
+meanings = [("maps","admits user-defined object mappings"),
+            ("list","respects list order"),
+            ("nest","supports data nesting"),
+            ("comp","has compositional syntax and semantics"),
+            ("aval","avoids query avalanches"),
+            ("type","is statically type-checked"),
+            ("SQL","guarantees translation to SQL")]
+            
+-- Haskell version:
+
+hasFeatures :: String -> [String] 
+hasFeatures f = [feat | (fac,feat) <- features, fac == f]
+
+means :: String -> String
+means f = head [mean | (feat,mean) <- meanings, feat == f ]
+
+query :: [(String , [String ])] 
+query = [(the cat, nub $ concat $ map (map means . hasFeatures) fac) 
+        | (fac, cat) <- facilities, then group by cat]
+              
