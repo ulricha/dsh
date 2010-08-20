78cce7a8e9a3e29120dcac8ef8dd1a9fbbb77585
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-08-20 08:17:21 +0200

    Example from paper in plain haskell and in Ferry.
    The ferry version lacks duplicate removal (there is no nub
    combinator yet). Other then that it works perfect :).

---
+++ examples/PaperExampleF1.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE ViewPatterns, QuasiQuotes, TemplateHaskell, TransformListComp #-}
+module PaperExampleF1 where
+    
+import Ferry (toQ, fromQ, Q (..), view, qc)
+import qualified Ferry as Q
+import Database.HDBC.Sqlite3
+import GHC.Exts (the)
+import Data.List (nub)
+
+conn :: Connection
+conn = undefined
+
+type Facility = String
+type Cat      = String
+type Feature  = String
+type Meaning  = String
+
+facilities :: Q [(Facility, Cat)]
+facilities = toQ [ ("SQL", "QLA"),
+               ("ODBC", "API"),
+               ("LINQ", "LIN"),
+               ("Links", "LIN"),
+               ("Rails", "ORM"),
+               ("Ferry", "LIB"),
+               ("Kleisli", "QLA"),
+               ("ADO.NET", "ORM"),
+               ("HaskellDB", "LIB")]
+               
+features :: Q [(Facility, Feature)]
+features = toQ [("Kleisli","nest")
+          ,("Kleisli","comp")
+          ,("Kleisli","type")
+          ,("Links","comp")
+          ,("Links","type")
+          ,("Links","SQL")
+          ,("LINQ","nest")
+          ,("LINQ","comp")
+          ,("LINQ","type")
+          ,("HaskellDB","comp")
+          ,("HaskellDB","type")
+          ,("HaskellDB","SQL")
+          ,("SQL","aval")
+          ,("SQL","type")
+          ,("SQL","SQL")
+          ,("Rails","nest")
+          ,("Rails","maps")
+          ,("ADO.NET","maps")
+          ,("ADO.NET","comp")
+          ,("ADO.NET","type")
+          ,("Ferry","list")
+          ,("Ferry","nest")
+          ,("Ferry","comp")
+          ,("Ferry","aval")
+          ,("Ferry","type")
+          ,("Ferry","SQL")] 
+          
+meanings :: Q [(Feature, Meaning)]
+meanings = toQ [("maps","admits user-defined object mappings"),
+            ("list","respects list order"),
+            ("nest","supports data nesting"),
+            ("comp","has compositional syntax and semantics"),
+            ("aval","avoids query avalanches"),
+            ("type","is statically type-checked"),
+            ("SQL","guarantees translation to SQL")]
+            
+-- Haskell version:
+
+hasFeatures :: Q String -> Q [String] 
+hasFeatures f = [$qc|feat | (fac,feat) <- features, Q.eq fac f|]
+
+means :: Q String -> Q String
+means f = Q.head [$qc| mean | (feat,mean) <- meanings, Q.eq feat f |]
+
+-- Only need Q.nub combinator
+query :: IO [(String , [String ])] 
+query = fromQ conn [$qc| Q.fromView (Q.the cat, Q.concat $ Q.map (Q.map means . hasFeatures) fac) 
+        | (fac, cat) <- facilities, then group by cat |]
