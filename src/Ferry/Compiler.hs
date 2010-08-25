334c83dce6b3465183f22d5a3bc0a07af8111d61
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2010-08-25 14:43:20 +0200

    Monad for fresh names

--- src/Ferry/Compiler.hs
+++ src/Ferry/Compiler.hs
@@ -1,10 +1,40 @@
 module Ferry.Compiler (evaluate) where
 
 import Ferry.Data
+import Ferry.Syntax
+import Ferry.Compiler.Transform
 -- import Ferry.Impossible
 
 -- import Data.Convertible
 import Database.HDBC
+
+{-
+N monad, version of the state monad that can provide fresh variable names.
+-}
+newtype N a = N (State Int a)
+
+unwrapN :: N a -> State Int a
+unwrapN (N s) = s
+
+instance Functor N where
+    fmap f a = N $ fmap f $ unwrapN a
+
+instance Monad N where
+    s >>= m = N (unwrapN s >>= unwrapN . m)
+    return = N . return
+    
+instance Applicative N where
+  pure  = return
+  (<*>) = ap
+
+freshVar :: N String
+freshVar = N $ do
+                i <- get
+                put (i + 1)
+                return $ "__fv" ++ show i
+     
+runN :: N a -> a
+runN = fst . (flip runState 1) . unwrapN
 
 evaluate :: IConnection conn
          => conn                -- ^ The HDBC connection
