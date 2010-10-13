579618d4daa2f604828dd22988844a7f8670ea22
George Giorgidze <george.giorgidze@uni-tuebingen.de>
2010-10-13 11:50:59 +0200

    Removing type signatures from Example2;

--- examples/OnlineForums/Example2.hs
+++ examples/OnlineForums/Example2.hs
@@ -2,8 +2,6 @@
 
 module Main where
 
--- import qualified Prelude as P
-
 import Records
 
 import Prelude ()
@@ -12,42 +10,33 @@ import qualified Prelude as P
 import Database.DSH
 import Database.DSH.Compiler (fromQ)
 
-import Database.HDBC.PostgreSQL
-
 import Math.Statistics
--- import Graphics.Gnuplot.Simple
-
-import System.Cmd
 import qualified Data.List as L
 
 threads :: Q [Thread]
-threads = tableWithKeys "spiegelThreads" [["spiegelThreadUrl"]]
+threads = table "spiegelThreads"
 
 posts :: Q [Post]
-posts = tableWithKeys "spiegelPosts" [["spiegelPostUrl"]]
+posts = table "spiegelPosts"
 
 quotes :: Q [Quote]
 quotes = table "spiegelQuotes"
 
-threadsWithRating :: Q [Thread]
 threadsWithRating =
   [$qc| t
       | t <- threads
       , (spiegelThreadRatingQ t) > 0
   |]
 
-postQuotes :: Q Post -> Q [Quote]
 postQuotes post =
   [$qc| q
       | q <- quotes
       , spiegelPostUrlQ post == spiegelQuotePostUrlQ q
   |]
 
-containsQuotes :: Q Post -> Q Double
 containsQuotes post =
   (length (postQuotes post) > 0) ? (1,0)
 
-threadsAndPosts :: Q [ (Thread , [Post]) ]
 threadsAndPosts = 
   [$qc| fromView (the thread, post)
       | thread <- threadsWithRating
@@ -56,7 +45,6 @@ threadsAndPosts =
       , then group by thread
   |]
 
-threadInteractivityAndRatings :: Q [(Double,Double)]
 threadInteractivityAndRatings =
   [$qc| fromView (interactivity , rating)
       | (thread,posts) <- threadsAndPosts
@@ -64,17 +52,14 @@ threadInteractivityAndRatings =
       , let rating        = spiegelThreadRatingQ thread
   |]
 
-main :: IO ()
 main = do
   conn <- getConnection
   
   irs <- fromQ conn (threadInteractivityAndRatings)
   
-  P.putStr "Pearson Correlation = "
+  P.putStr "Correlation Coefficient = "
   P.print ((P.uncurry pearson) (P.unzip (L.sort irs)))
   
   -- plotPath [] (L.sort irs)
   
   P.return ()
-  
-  
