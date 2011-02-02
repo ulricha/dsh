6928a9389888db555f5256fce9724fdd1bed00ac
George Giorgidze <george.giorgidze@uni-tuebingen.de>
2011-02-02 01:22:11 +0100

    Preparing the release: all Double related test now pass.

--- DSH/tests/Main.hs
+++ DSH/tests/Main.hs
@@ -18,6 +18,9 @@ import GHC.Exts
 import qualified Data.Text as Text
 import Data.Text (Text)
 
+instance Arbitrary Text where
+  arbitrary = fmap Text.pack arbitrary
+
 getConn :: IO Connection
 getConn = connectPostgreSQL "user = 'postgres' password = 'haskell98' host = 'localhost' dbname = 'ferry'"
 
@@ -34,9 +37,8 @@ main = do
     -- quickCheck prop_char
     putStr "Integer:        "
     quickCheck prop_integer
-    -- Precision issues G
-    -- putStr "Double:         "
-    -- quickCheck prop_double
+    putStr "Double:         "
+    quickCheck prop_double
     -- Encoding Issues G
     -- putStr "Text:           "
     -- quickCheck prop_text
@@ -127,9 +129,8 @@ main = do
     quickCheck prop_all_zero
     putStr "sum_integer:    "
     quickCheck prop_sum_integer
-    -- Precision Issue G
-    -- putStr "sum_double:     "
-    -- quickCheck prop_sum_double
+    putStr "sum_double:     "
+    quickCheck prop_sum_double
     -- Remove G
     -- putStr "product_integer:"
     -- quickCheck prop_product_integer
@@ -208,51 +209,63 @@ main = do
     putStrLn "-------------------------"
     putStr "add_integer:    "
     quickCheck prop_add_integer
-    -- Precision Issue G
-    -- putStr "add_double:     "
-    -- quickCheck prop_add_double
+    putStr "add_double:     "
+    quickCheck prop_add_double
     putStr "mul_integer:    "
     quickCheck prop_mul_integer
-    -- Precision Issue G
-    -- putStr "mul_double:     "
-    -- quickCheck prop_mul_double
-    -- putStr "div_double:     "
-    -- quickCheck prop_div_double
-    -- putStr "integer_to_double: "
-    -- quickCheck prop_integer_to_double    
+    putStr "mul_double:     "
+    quickCheck prop_mul_double
+    putStr "div_double:     "
+    quickCheck prop_div_double
+    putStr "integer_to_double: "
+    quickCheck prop_integer_to_double    
     putStr "abs_integer:    "
     quickCheck prop_abs_integer
-    -- putStr "abs_double:     "
-    -- quickCheck prop_abs_double
+    putStr "abs_double:     "
+    quickCheck prop_abs_double
     putStr "signum_integer: "
     quickCheck prop_signum_integer
-    -- putStr "signum_double:  "
-    -- quickCheck prop_signum_double
+    putStr "signum_double:  "
+    quickCheck prop_signum_double
     putStr "negate_integer: "
     quickCheck prop_negate_integer
-    -- putStr "negate_double:  "
-    -- quickCheck prop_negate_double
+    putStr "negate_double:  "
+    quickCheck prop_negate_double
 
 
-runTest :: (Eq b, QA a, QA b)
+runTest :: (Eq b, QA a, QA b, Show a, Show b)
         => (Q a -> Q b)
         -> (a -> b)
         -> a
         -> Property
 runTest q f arg = monadicIO $ do
     c  <- run $ getConn
-    it <- run $ fromQ c (q (Q.toQ arg))
+    db <- run $ fromQ c (q (Q.toQ arg))
     run $ HDBC.disconnect c
-    let hs = f arg
-    assert (it == hs) -- (db == hs && it == hs)
+    let hs = f arg    
+    assert (db == hs)
 
--- | Test if the list isn't empty
-testNotNull :: (Eq b, Q.QA a, Q.QA b)
+testNotNull :: (Eq b, Q.QA a, Q.QA b, Show a, Show b)
             => (Q.Q [a] -> Q.Q b)
             -> ([a] -> b)
             -> [a]
             -> Property
 testNotNull q f arg = not (null arg) ==> runTest q f arg
+
+runTestDouble :: (QA a, Show a)
+        => (Q a -> Q Double)
+        -> (a -> Double)
+        -> a
+        -> Property
+runTestDouble q f arg = monadicIO $ do
+    c  <- run $ getConn
+    db <- run $ fromQ c (q (Q.toQ arg))
+    run $ HDBC.disconnect c
+    let hs = f arg
+    let eps = 1.0E-6 :: Double;    
+    assert (abs(db - hs) < eps)
+
+
 
 
 uncurry_Q :: (Q.QA a, Q.QA b) => (Q.Q a -> Q.Q b -> Q.Q c) -> Q.Q (a,b) -> Q.Q c
@@ -271,7 +284,7 @@ prop_integer :: Integer -> Property
 prop_integer = runTest id id
 
 prop_double :: Double -> Property
-prop_double = runTest id id
+prop_double = runTestDouble id id
 
 prop_text :: Text -> Property
 prop_text = runTest id id
@@ -290,13 +303,13 @@ prop_infix_or = runTest (uncurry_Q (Q.||)) (uncurry (||))
 prop_not :: Bool -> Property
 prop_not = runTest Q.not not
 
-prop_eq :: (Eq a, Q.QA a) => (a,a) -> Property
+prop_eq :: (Eq a, Q.QA a, Show a) => (a,a) -> Property
 prop_eq = runTest (\q -> Q.fst q Q.== Q.snd q) (\(a,b) -> a == b)
 
 prop_eq_int :: (Integer,Integer) -> Property
 prop_eq_int = prop_eq
 
-prop_neq :: (Eq a, Q.QA a) => (a,a) -> Property
+prop_neq :: (Eq a, Q.QA a, Show a) => (a,a) -> Property
 prop_neq = runTest (uncurry_Q (Q./=)) (\(a,b) -> a /= b)
 
 prop_neq_int :: (Integer,Integer) -> Property
@@ -321,10 +334,10 @@ prop_max_integer :: (Integer,Integer) -> Property
 prop_max_integer = runTest (uncurry_Q Q.max) (uncurry max)
 
 prop_min_double :: (Double,Double) -> Property
-prop_min_double = runTest (uncurry_Q Q.min) (uncurry min)
+prop_min_double = runTestDouble (uncurry_Q Q.min) (uncurry min)
 
 prop_max_double :: (Double,Double) -> Property
-prop_max_double = runTest (uncurry_Q Q.max) (uncurry max)
+prop_max_double = runTestDouble (uncurry_Q Q.max) (uncurry max)
 
 --------------------------------------------------------------------------------
 -- Lists
@@ -421,7 +434,7 @@ prop_sum_integer :: [Integer] -> Property
 prop_sum_integer = runTest Q.sum sum
 
 prop_sum_double :: [Double] -> Property
-prop_sum_double = runTest Q.sum sum
+prop_sum_double = runTestDouble Q.sum sum
 
 prop_product_integer :: [Integer] -> Property
 prop_product_integer = runTest Q.product product
@@ -524,41 +537,36 @@ prop_add_integer :: (Integer,Integer) -> Property
 prop_add_integer = runTest (uncurry_Q (+)) (uncurry (+))
 
 prop_add_double :: (Double,Double) -> Property
-prop_add_double = runTest (uncurry_Q (+)) (uncurry (+))
+prop_add_double = runTestDouble (uncurry_Q (+)) (uncurry (+))
 
 prop_mul_integer :: (Integer,Integer) -> Property
 prop_mul_integer = runTest (uncurry_Q (*)) (uncurry (*))
 
 prop_mul_double :: (Double,Double) -> Property
-prop_mul_double = runTest (uncurry_Q (*)) (uncurry (*))
+prop_mul_double = runTestDouble (uncurry_Q (*)) (uncurry (*))
 
 prop_div_double :: (Double,Double) -> Property
 prop_div_double (x,y) =
       y /= 0
-  ==> runTest (uncurry_Q (/)) (uncurry (/)) (x,y)
+  ==> runTestDouble (uncurry_Q (/)) (uncurry (/)) (x,y)
 
 prop_integer_to_double :: Integer -> Property
-prop_integer_to_double = runTest Q.integerToDouble fromInteger
+prop_integer_to_double = runTestDouble Q.integerToDouble fromInteger
 
 prop_abs_integer :: Integer -> Property
 prop_abs_integer = runTest Q.abs abs
 
 prop_abs_double :: Double -> Property
-prop_abs_double = runTest Q.abs abs
+prop_abs_double = runTestDouble Q.abs abs
 
 prop_signum_integer :: Integer -> Property
 prop_signum_integer = runTest Q.signum signum
 
 prop_signum_double :: Double -> Property
-prop_signum_double = runTest Q.signum signum
+prop_signum_double = runTestDouble Q.signum signum
 
 prop_negate_integer :: Integer -> Property
 prop_negate_integer = runTest Q.negate negate
 
 prop_negate_double :: Double -> Property
-prop_negate_double = runTest Q.negate negate
-
-
-
-instance Arbitrary Text where
-  arbitrary = fmap Text.pack arbitrary
+prop_negate_double = runTestDouble Q.negate negate
