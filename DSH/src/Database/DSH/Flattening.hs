a308257f5bf3f44b70ee24b23f5b824f1799fbc7
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2011-03-10 08:29:36 +0100

    Small start, had to check in in order to switch to master branch

--- DSH/src/Database/DSH/Flattening.hs
+++ DSH/src/Database/DSH/Flattening.hs
@@ -1,2 +1,10 @@
-module Database.DSH.Flattening where
+-- | This module provides the flattening implementation of DSH.
+module Database.DSH.Flattening (fromQ) where
+    
+import Database.DSH.Data as D
+import Database.DSH.Impossible (impossible)
+import Database.HDBC
+
+fromQ :: (QA a, IConnection conn) => conn -> Q a -> IO a
+fromQ c (Q a) = evaluate c a >>= (return . fromNorm)
     
