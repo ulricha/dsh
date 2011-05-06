774a16b9c0418ed8692513716651f4e698143001
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2011-05-06 15:12:39 +0200

    Defining dist over closures

--- DBPH/src/Language/ParallelLang/VL/Data/Query.hs
+++ DBPH/src/Language/ParallelLang/VL/Data/Query.hs
@@ -1,6 +1,9 @@
 module Language.ParallelLang.VL.Data.Query where
 
 import Language.ParallelLang.FKL.Render.Render()
+import Language.ParallelLang.FKL.Data.FKL
+import qualified Language.ParallelLang.Common.Data.Type as T
+
 
 data Query a =
          TupleVector [Query a]
@@ -9,8 +12,14 @@ data Query a =
        | PrimVal a
        | NestedVector a (Query a)
        | PropVector a
---       | UnEvaluated (Expr T.VType)
+       | Closure String [(String, Query a)] (Expr T.Type) (Expr T.Type)
+       | AClosure [(String, Query a)] (Expr T.Type) (Expr T.Type)
      deriving Show
+      
+nestingDepth :: Query a -> Int
+nestingDepth (ValueVector _) = 1
+nestingDepth (NestedVector _ r) = 1 + nestingDepth r
+nestingDepth _                  = error "Not a list vector"
        
 data XML = XML Int String
 
