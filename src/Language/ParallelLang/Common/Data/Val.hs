cd95ce8918f8915e482e9b137932b266561cf467
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2011-07-01 16:09:03 +0200

    Working on optimising literal lists

--- src/Language/ParallelLang/Common/Data/Val.hs
+++ src/Language/ParallelLang/Common/Data/Val.hs
@@ -5,6 +5,7 @@ module Language.ParallelLang.Common.Data.Val where
 Basic values in both FKL and NKL. 
 -}
 data Val where
+    List :: [Val] -> Val
     Int :: Int -> Val
     Bool :: Bool -> Val
     String :: String -> Val
