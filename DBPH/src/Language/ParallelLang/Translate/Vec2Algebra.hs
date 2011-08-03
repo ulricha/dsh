cc54e6ec382bb75fac86ad5d9d9263afb1429b25
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2011-08-03 13:15:29 +0200

    Fix for initloop value

--- DBPH/src/Language/ParallelLang/Translate/Vec2Algebra.hs
+++ DBPH/src/Language/ParallelLang/Translate/Vec2Algebra.hs
@@ -17,6 +17,8 @@ import Database.Algebra.Pathfinder.Render.XML hiding (XML, Graph)
 import qualified Language.ParallelLang.Common.Data.Type as Ty
 import Language.ParallelLang.VL.VectorOperations
 
+import Database.Algebra.Pathfinder(initLoop)
+
 import qualified Data.Map as M
 import Control.Applicative hiding (Const)
 import Control.Monad (liftM2)
@@ -35,7 +37,7 @@ fkl2Alg (Nil (Ty.List t@(Ty.List _))) = do
 fkl2Alg (Nil (Ty.List t)) = do
   p_empty <- emptyVector [(descr, Ty.Nat), (pos, Ty.Nat), (item1, t)]
   return (ValueVector p_empty)
-fkl2Alg (Nil _)                = error "Not a valid nil value"
+fkl2Alg (Nil t)                = error $ "Not a valid nil value" ++ show t
 fkl2Alg (BinOp _ (Op o l) e1 e2) | o == Cons = do
                                                 p1 <- fkl2Alg e1
                                                 p2 <- fkl2Alg e2
@@ -105,7 +107,7 @@ fkl2Alg (CloLApp _ c arg) = do
 -- loop relations. At some point, loop should be separated from the general
 -- GraphM monad.                              
 toPFAlgebra :: Expr Ty.Type -> AlgPlan PFAlgebra Plan
-toPFAlgebra e = runGraph undefined (fkl2Alg e)
+toPFAlgebra e = runGraph initLoop (fkl2Alg e)
 
 --toX100Algebra :: Expr Ty.Type -> AlgPlan X100Algebra Plan
 --toX100Algebra e = runGraph initialLoop (fkl2Alg e)
