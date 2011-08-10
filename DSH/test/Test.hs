a0f4af2e2dd59db95418ba689b84ad989401ac06
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2011-08-10 11:47:03 +0200

    Test file

--- DSH/test/Test.hs
+++ DSH/test/Test.hs
@@ -1,20 +1,59 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
 module Test where
 
-import qualified Database.DSH as Q
+import Prelude()
+import Database.DSH
 import qualified Database.DSH.Flattening as I
+import qualified Database.DSH.Compiler as C
 
 import qualified Database.HDBC as HDBC
 import Database.HDBC.PostgreSQL
 
+import Data.Text (Text(..))
+
 getConn :: IO Connection
 getConn = connectPostgreSQL "user = 'postgres' password = 'haskell98' host = 'localhost' dbname = 'ferry'"
 
+t :: IO ()
+t = do
+     conn <- getConn
+     q <- I.fromQ conn $ groupWith (\(view -> (n, d, i)) -> d) employees
+     putStrLn $ show q
+{-
+test :: IO ()
+test = do 
+        conn <- getConn
+        q <- I.debugPlan conn $ query
+        putStrLn $ q    
+-}
+employees :: Q [(Text, Text, Integer)]
+employees = toQ [
+    ("Simon",  "MS",   80)
+  , ("Erik",   "MS",   90)
+  , ("Phil",   "Ed",   40)
+  , ("Gordon", "Ed",   45)
+  , ("Paul",   "Yale", 60)
+  ]
+
+{-
+employees :: Q [(Text, Text)]
+employees = toQ [("MS", "Bla")]
+-}
+
+{-
+query :: Q [[Text]]
+query = [$qc| dept
+            | (dept, burp) <- employees
+            , then group by dept
+            |]
+-}
+{-
 test :: IO ()
 test = do
          conn <- getConn
          val <- I.fromQ conn $ Q.toQ [(1::Integer,2::Integer),(3,4),(5,6)]
          putStrLn $ show val
-
+-}
 {-
 test :: IO ()
 test = do
