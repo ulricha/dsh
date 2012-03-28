1680e35e542c9670c7fa80b9dc74cd6f2dbb53aa
Jeroen Weijers <jeroen.weijers@uni-tuebingen.de>
2012-03-28 11:00:29 +0200

    Improving documentation, simplifying the flattening transformation

--- DBPH/src/Language/ParallelLang/Translate/TransM.hs
+++ DBPH/src/Language/ParallelLang/Translate/TransM.hs
@@ -5,57 +5,20 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.State
 
-newtype TransM a = TransM (State (Int, [String]) a)
+newtype TransM a = TransM (State Int a)
     deriving (Monad, Functor)
 
-deriving instance MonadState (Int, [String]) TransM
+deriving instance MonadState Int TransM
 
 getFreshVar :: TransM String
 getFreshVar = do
-                (v, l) <- get
-                put (v + 1, l)
+                v <- get
+                put $ v + 1
                 return $ "***_FV" ++ show v
 
 instance Applicative TransM where
     pure  = return
     (<*>) = ap
 
-withClosure :: [String] -> TransM a -> TransM a
-withClosure xs e = do
-                    (v, l) <- get
-                    put (v, xs)
-                    e' <- e
-                    (v', _) <- get
-                    put (v', l)
-                    return e'
-
-withCleanClosureEnv :: TransM a -> TransM a
-withCleanClosureEnv e = do
-                     (v, l) <- get
-                     put (v, [])
-                     e' <- e
-                     (v', _) <- get
-                     put (v', l)
-                     return e'
-
-withVar :: String -> TransM a -> TransM a
-withVar n e = do
-                    (v, l) <- get
-                    put (v, n:l)
-                    e' <- e
-                    (v', _) <- get
-                    put (v', l)
-                    return e'
-
-isClosureVar :: String -> TransM Bool
-isClosureVar v = do 
-                (_, vs) <- get
-                return $ elem v vs
-                
-getClosureVars :: TransM [String]
-getClosureVars = do
-               (_, vs) <- get
-               return vs
-               
 runTransform :: TransM a -> a
-runTransform (TransM e) = fst $ flip runState (0, []) e
+runTransform (TransM e) = fst $ flip runState 0 e
