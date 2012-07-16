9532b38d842989566b54a77cbcd2fcd25315c211
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-07-16 16:13:48 +0200

    New rewrite Redundant.PullRestrictThroughPair: Pull two RestrictVecs through a pair constructor if both RestrictVecs use the same boolean vector and their left inputs are from the same source.

--- DSH/src/Database/DSH/Flattening/Optimizer/VL/Rewrite/Redundant.hs
+++ DSH/src/Database/DSH/Flattening/Optimizer/VL/Rewrite/Redundant.hs
@@ -2,6 +2,10 @@
 
 module Optimizer.VL.Rewrite.Redundant where
 
+import Text.Printf
+import Debug.Trace
+
+import Control.Monad
 import qualified Data.Map as M
 
 import Database.Algebra.Rewrite
@@ -9,11 +13,13 @@ import Database.Algebra.Dag.Common
 import Database.Algebra.VL.Data
   
 removeRedundancy :: DagRewrite VL Bool
-removeRedundancy = preOrder (return M.empty) redundantRules
+removeRedundancy = iteratively $ preOrder (return M.empty) redundantRules
 
 redundantRules :: RuleSet VL ()
 redundantRules = [ mergeStackedDistDesc 
-                 , restrictCombine ]
+                 , restrictCombineDBV 
+                 , restrictCombinePropLeft
+                 , pullRestrictThroughPair ]
 
 mergeStackedDistDesc :: Rule VL ()
 mergeStackedDistDesc q = 
@@ -24,12 +30,45 @@ mergeStackedDistDesc q =
           logRewriteM "Redundant.MergeStackedDistDesc" q
           relinkParentsM q $(v "first") |])
   
-restrictCombine :: Rule VL ()
-restrictCombine q =
+restrictCombineDBV :: Rule VL ()
+restrictCombineDBV q =
   $(pattern [| q |] "R1 (CombineVec (qb1) (ToDescr (R1 ((q1) RestrictVec (qb2)))) (ToDescr (R1 ((q2) RestrictVec (NotVec (qb3))))))"
     [| do
         predicate $ $(v "q1") == $(v "q2")
         predicate $ $(v "qb1") == $(v "qb2") && $(v "qb1") == $(v "qb3")
         return $ do
-          logRewriteM "Redundant.RestrictCombine" q
+          logRewriteM "Redundant.RestrictCombine.DBV" q
           relinkParentsM q $(v "q1") |])
+
+restrictCombinePropLeft :: Rule VL ()
+restrictCombinePropLeft q =
+  $(pattern [| q |] "R2 (c=CombineVec (qb) (a) (b))"
+    [| do
+        parentNodes <- parents $(v "c")
+        parentOps <- mapM operator parentNodes
+        let r1 = filter (\(_, op) -> case op of { UnOp R1 _ -> True; _ -> False }) $ zip parentNodes parentOps
+        r1Parents <-
+          case r1 of
+            [(r1Node, _)] -> liftM length $ parents r1Node
+            _ -> fail ""
+        predicate $ r1Parents == 0
+
+        return $ do
+          logRewriteM "Redundant.RestrictCombine.PropLeft" q
+          selectNode <- insertM $ UnOp SelectItem $(v "qb") 
+          projectNode <- insertM $ UnOp (ProjectRename PosCol Number) selectNode
+          relinkParentsM q projectNode |])
+  
+pullRestrictThroughPair :: Rule VL ()
+pullRestrictThroughPair q =
+  $(pattern [| q |] "(R1 ((qp1=ProjectL _ (q1)) RestrictVec (qb1))) PairL (R1 ((qp2=ProjectL _ (q2)) RestrictVec (qb2)))"
+    [| do
+        predicate $ $(v "qb1") == $(v "qb2")
+        predicate $ $(v "q1") == $(v "q2")
+        
+        return $ do
+          logRewriteM "Redundant.PullRestrictThroughPair" q
+          pairNode <- insertM $ BinOp PairL $(v "qp1") $(v "qp2")
+          restrictNode <- insertM $ BinOp RestrictVec pairNode $(v "qb1")
+          r1Node <- insertM $ UnOp R1 restrictNode
+          relinkParentsM q r1Node |])
