fa010174da7bb25b89e1ce373583c8af6b75e8fd
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-07-30 15:09:44 +0200

    New rewrite: try to find a chain of descriptor modifiers s.t. the descriptor is constant at the start and at the end. This chain is then a no-op and can be removed.

--- DSH/src/Database/DSH/Flattening/Optimizer/VL/Rewrite/DescriptorModifiers.hs
+++ DSH/src/Database/DSH/Flattening/Optimizer/VL/Rewrite/DescriptorModifiers.hs
@@ -2,6 +2,8 @@
 
 module Optimizer.VL.Rewrite.DescriptorModifiers where
 
+import Debug.Trace
+
 import Control.Monad
 
 import Optimizer.VL.Properties.Types
@@ -13,77 +15,36 @@ import Database.Algebra.Dag.Common
 import Database.Algebra.VL.Data
   
 stripFromRoot :: DagRewrite VL Bool
-stripFromRoot = iteratively $ preOrder inferBottomUp stripRootRules
+stripFromRoot = iteratively $ preOrder inferBottomUp descriptorNoOps
 
-stripRootRules :: RuleSet VL BottomUpProps
-stripRootRules = [ stripFlatRootPropRename
-                 , stripFlatRootPropRenameProject
-                 , stripFlatRootSegment
-                 , stripFlatRootSegmentProject ]
+descriptorNoOps :: RuleSet VL BottomUpProps
+descriptorNoOps = [ constantDescriptorChain ]
                  
-stripFlatRootPropRename :: Rule VL BottomUpProps
-stripFlatRootPropRename q =
+hasConstDesc :: VectorProp ConstVec -> Bool
+hasConstDesc (VProp (DBVConst (ConstDescr _) _))      = True
+hasConstDesc (VProp (DescrVecConst (ConstDescr _)))   = True
+hasConstDesc _                                        = False
+                 
+traverseChain :: AlgNode -> Match VL BottomUpProps AlgNode
+traverseChain q = do
+  op <- operator q
+  case op of
+    BinOp PropRename _ c2 -> trace ("at " ++ (show q)) $ traverseChain c2
+    UnOp Segment c -> trace ("at " ++ (show q)) $ traverseChain c
+    _ -> do
+      trace ("at " ++ (show q)) $ predicateM $ liftM (hasConstDesc . constProp) $ properties q
+      return q
+                 
+{- Try to find a chain of descriptor-modifying operators (e.g. PropRename, Segment) which
+form a noop because the desccriptor is constant at the beginning of the chain and at the end. -}
+constantDescriptorChain :: Rule VL BottomUpProps
+constantDescriptorChain q = 
   $(pattern [| q |] "(_) PropRename (qv)"
     [| do
-        rs <- rootNodes
-        predicate $ (length rs) == 1
-        predicate $ q `elem` rs
-        width <- liftM (schemaWidth . vectorSchemaProp) $ properties $(v "qv")
+        trace ("top " ++ (show q)) $ predicateM $ liftM (hasConstDesc . constProp) $ properties q
+        chainStart <- traverseChain $(v "qv")
         
-        return $ do
-          logRewriteM "DescriptorModifiers.FlatRootPropRename" q
-          let valProjs = map PLCol [1..width]
-              projectOp = UnOp (ProjectValue (DescrConst (N 1), PosIdentity, valProjs)) $(v "qv")
-          replaceM q projectOp |])
-
--- FIXME duplicatin the rule like this to account for the introduced root projection is horrible.
--- Instead, the code should walk the DAG from the top rightwards, search for a chain of
--- descriptor modifiers and then remove the complete chain.
-stripFlatRootPropRenameProject :: Rule VL BottomUpProps
-stripFlatRootPropRenameProject q =
-  $(pattern [| q |] "ProjectValue ps (qpr=(_) PropRename (qv))"
-    [| do
-        let (descrProj, _, _) = $(v "ps")
-        predicate $ case descrProj of
-          DescrConst (N 1) -> True
-          _                -> False
-        rs <- rootNodes
-        predicate $ (length rs) == 1
-        predicate $ q `elem` rs
-        
-        return $ do
-          logRewriteM "DescriptorModifiers.FlatRootPropRename" q
-          valueOp <- operatorM $(v "qv")
-          replaceM $(v "qpr") valueOp |])
-
-stripFlatRootSegment :: Rule VL BottomUpProps
-stripFlatRootSegment q =
-  $(pattern [| q |] "Segment (qv)"
-    [| do
-        rs <- rootNodes
-        predicate $ (length rs) == 1
-        predicate $ q `elem` rs
-        width <- liftM (schemaWidth . vectorSchemaProp) $ properties $(v "qv")
-        
-        return $ do
-          logRewriteM "DescriptorModifiers.FlatRootSegment" q
-          let valProjs = map PLCol [1..width]
-              projectOp = UnOp (ProjectValue (DescrConst (N 1), PosIdentity, valProjs)) $(v "qv")
-          replaceM q projectOp |])
-
-stripFlatRootSegmentProject :: Rule VL BottomUpProps
-stripFlatRootSegmentProject q =
-  $(pattern [| q |] "ProjectValue ps (qs=Segment (qv))"
-    [| do
-        let (descrProj, _, _) = $(v "ps")
-        predicate $ case descrProj of
-          DescrConst (N 1) -> True
-          _                -> False
-        rs <- rootNodes
-        predicate $ (length rs) == 1
-        predicate $ q `elem` rs
-        
-        return $ do
-          logRewriteM "DescriptorModifiers.FlatRootSegment" q
-          valueOp <- operatorM $(v "qv")
-          replaceM $(v "qs") valueOp |])
+        trace "match" $ return $ do
+          logRewriteM "DescriptorModifiers.ConstantDescriptorChain" q
+          op <- operatorM chainStart
+          replaceM q op |])
