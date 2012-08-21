b7e74d9d3440e6a5c3615c48c603c19c32658ed0
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-08-21 18:09:26 +0200

    Redundant code in testcase removed.

--- tests/spiegel/Records.hs
+++ tests/spiegel/Records.hs
@@ -7,16 +7,12 @@ import Control.Exception
 import Database.DSH
 import Database.DSH.Compiler
 
-import Database.HDBC.PostgreSQL
 import Database.X100Client
 
 #define getConnectionDef (connectPostgreSQL "user = 'postgres' password='haskell98' host = 'localhost' dbname = 'onlineForums'")
 
 #define x100Conn (Prelude.return $ x100Info "localhost" "48130" Nothing)
   
-getConnection :: IO Connection
-getConnection = getConnectionDef
-
 $(generateX100TableRecordInstances x100Conn "spiegel_users"   "User"   [''Show,''Eq,''Ord])
 $(generateX100TableRecordInstances x100Conn "spiegel_threads" "Thread" [''Show,''Eq,''Ord])
 $(generateX100TableRecordInstances x100Conn "spiegel_posts"   "Post"   [''Show,''Eq,''Ord])
@@ -33,15 +29,3 @@ posts = tableWithKeys "spiegel_posts" [["spiegel_post_url"]]
 
 quotes :: Q [Quote]
 quotes = table "spiegel_quotes"
-
-runQ :: (QA a) => Q a -> IO a
-runQ q =
-  bracket getConnection
-          disconnect
-          (\conn -> fromQ conn q)
-
-debugQ :: (QA a) => Q a -> IO String
-debugQ q =
-  bracket getConnection
-          disconnect
-          (\conn -> debugSQL conn q)
