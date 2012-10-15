4085bde7c7bef65556449cf1665e852857fdd2fe
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-10-15 14:53:48 +0200

    Cleanup

--- DSH/src/Database/DSH/Flattening/Optimizer/VL/OptimizeVL.hs
+++ DSH/src/Database/DSH/Flattening/Optimizer/VL/OptimizeVL.hs
@@ -1,30 +1,29 @@
 module Optimizer.VL.OptimizeVL where
 
-import Data.Functor
-import Control.Applicative
+import           Control.Applicative
 
-import qualified Database.Algebra.Dag as Dag
-import Database.Algebra.Rewrite
-import Database.Algebra.VL.Data
+import qualified Database.Algebra.Dag                     as Dag
+import           Database.Algebra.Rewrite
+import           Database.Algebra.VL.Data
 
-import Optimizer.Common.Rewrite
-import Optimizer.Common.Shape
-import Optimizer.VL.Rewrite.PruneEmpty
-import Optimizer.VL.Rewrite.MergeProjections
-import Optimizer.VL.Rewrite.Redundant
-import Optimizer.VL.Rewrite.Specialized
-import Optimizer.VL.Rewrite.Expressions
-import Optimizer.VL.Rewrite.DescriptorModifiers
-import Optimizer.VL.Rewrite.ToDescr
+import           Optimizer.Common.Rewrite
+import           Optimizer.Common.Shape
+import           Optimizer.VL.Rewrite.DescriptorModifiers
+import           Optimizer.VL.Rewrite.Expressions
+import           Optimizer.VL.Rewrite.MergeProjections
+import           Optimizer.VL.Rewrite.PruneEmpty
+import           Optimizer.VL.Rewrite.Redundant
+import           Optimizer.VL.Rewrite.Specialized
+import           Optimizer.VL.Rewrite.ToDescr
 
 type RewriteClass = OptRewrite VL Bool
 
 rewriteClasses :: [(Char, RewriteClass)]
-rewriteClasses = [ ('E', pruneEmpty) 
+rewriteClasses = [ ('E', pruneEmpty)
                  , ('P', mergeProjections)
-                 , ('R', removeRedundancy) 
+                 , ('R', removeRedundancy)
                  , ('C', optExpressions)
-                 , ('S', introduceSpecializedOperators) 
+                 , ('S', introduceSpecializedOperators)
                  , ('T', pruneColumns)
                  , ('D', stripFromRoot) ]
 
@@ -32,10 +31,10 @@ defaultPipeline :: [RewriteClass]
 defaultPipeline = case assemblePipeline "ERSRSD" of
   Just p -> p
   Nothing -> error "invalid default pipeline"
-  
+
 cleanup :: OptRewrite VL Bool
 cleanup = pruneUnused >> return True
-  
+
 runPipeline :: Dag.AlgebraDag VL -> Shape -> [RewriteClass] -> (Dag.AlgebraDag VL, Log, Shape)
 runPipeline d sh pipeline = (d', rewriteLog, sh')
   where (d', sh', rewriteLog) = runOptRewrite (sequence_ pipeline) sh d
