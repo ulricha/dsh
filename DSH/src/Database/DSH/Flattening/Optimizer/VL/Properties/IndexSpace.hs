d63612b66e47708da4a40eb429205498e906efdf
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2012-12-18 16:05:07 +0100

    Another embarassing bug fixed.

--- DSH/src/Database/DSH/Flattening/Optimizer/VL/Properties/IndexSpace.hs
+++ DSH/src/Database/DSH/Flattening/Optimizer/VL/Properties/IndexSpace.hs
@@ -1,23 +1,23 @@
 module Database.DSH.Flattening.Optimizer.VL.Properties.IndexSpace where
 
-import Database.Algebra.Dag.Common
-import Database.Algebra.VL.Data
+import           Database.Algebra.Dag.Common
+import           Database.Algebra.VL.Data
 
-import Database.DSH.Flattening.Optimizer.VL.Properties.Common
-import Database.DSH.Flattening.Optimizer.VL.Properties.Types
-import Database.DSH.Flattening.Optimizer.VL.Properties.AbstractDomains
+import           Database.DSH.Flattening.Optimizer.VL.Properties.AbstractDomains
+import           Database.DSH.Flattening.Optimizer.VL.Properties.Common
+import           Database.DSH.Flattening.Optimizer.VL.Properties.Types
 
 unp :: Show a => VectorProp a -> Either String a
 unp = unpack "Properties.IndexSpace"
-      
+
 fromDBV :: IndexSpace -> Either String (DescrIndexSpace, PosIndexSpace)
 fromDBV (DBVSpace dis pis)         = Right (dis, pis)
 fromDBV (DescrVectorSpace dis pis) = Right (dis, pis)
 fromDBV _                          = Left "IndexSpace.fromDBV: not a Value vector/descriptor vector"
-        
+
 fromRenameVector :: IndexSpace -> Either String (SourceIndexSpace, TargetIndexSpace)
 fromRenameVector (RenameVectorTransform sis tis) = Right (sis, tis)
-fromRenameVector _                               = Left "IndexSpace.fromRenameVector: not a rename vector"
+fromRenameVector x                               = Left $ "IndexSpace.fromRenameVector: not a rename vector" ++ (show x)
 
 -- Non-Either IndexSpace accessors
 descrSpaceDBV :: VectorProp IndexSpace -> Domain
@@ -34,38 +34,38 @@ freshSpace n c = makeSubDomain n c UniverseDom
 freshSpaces :: AlgNode -> (DescrIndexSpace, PosIndexSpace)
 freshSpaces n =
   (D $ freshSpace n "d", P $ freshSpace n "p")
-  
+
 freshDBVSpace :: AlgNode -> IndexSpace
 freshDBVSpace n = uncurry DBVSpace $ freshSpaces n
-                  
+
 freshTransformSpaces :: AlgNode -> (SourceIndexSpace, TargetIndexSpace)
-freshTransformSpaces n = 
+freshTransformSpaces n =
   (S $ makeSubDomain n "s" UniverseDom, T $ makeSubDomain n "t" UniverseDom)
-  
+
 freshPropSpace :: AlgNode -> IndexSpace
 freshPropSpace n = uncurry PropVectorTransform $ freshTransformSpaces n
-  
+
 freshValuePropPair :: AlgNode -> (IndexSpace, IndexSpace)
-freshValuePropPair n = 
+freshValuePropPair n =
   (freshDBVSpace n, uncurry PropVectorTransform $ freshTransformSpaces n)
-                    
+
 freshValueRenamePair :: AlgNode -> (IndexSpace, IndexSpace)
 freshValueRenamePair n =
   (freshDBVSpace n, uncurry RenameVectorTransform $ freshTransformSpaces n)
-  
+
 inferIndexSpaceNullOp :: AlgNode -> NullOp -> Either String (VectorProp IndexSpace)
-inferIndexSpaceNullOp n op = 
+inferIndexSpaceNullOp n op =
   case op of
     SingletonDescr              -> Right $ VProp $ uncurry DescrVectorSpace $ freshSpaces n
     ConstructLiteralTable _ _   -> Right $ VProp $ freshDBVSpace n
     ConstructLiteralValue _ _   -> Right $ VProp $ DBPSpace $ snd $ freshSpaces n
     TableRef              _ _ _ -> Right $ VProp $ freshDBVSpace n
 
-inferIndexSpaceUnOp :: VectorProp IndexSpace 
-                       -> AlgNode 
-                       -> UnOp 
+inferIndexSpaceUnOp :: VectorProp IndexSpace
+                       -> AlgNode
+                       -> UnOp
                        -> Either String (VectorProp IndexSpace)
-inferIndexSpaceUnOp is n op = 
+inferIndexSpaceUnOp is n op =
   case op of
     Unique -> Right $ VProp $ freshDBVSpace n
     UniqueL -> Right $ VProp $ freshDBVSpace n
@@ -75,20 +75,20 @@ inferIndexSpaceUnOp is n op =
     DescToRename -> do
       ((D dis), (P pis)) <- unp is >>= fromDBV
       return $ VProp $ RenameVectorTransform (S pis) (T dis)
-    
+
     ToDescr -> do
       (dis, pis) <- unp is >>= fromDBV
       return $ VProp $ DescrVectorSpace dis pis
-      
+
     Segment -> do
       (_, (P pis)) <- unp is >>= fromDBV
       return $ VProp $ DBVSpace (D pis) (P pis)
-      
+
     Unsegment -> do
       (_, pis) <- unp is >>= fromDBV
       return $ VProp $ DBVSpace (D $ freshSpace n "d") pis
-      
-    VecSum _ -> Right $ VProp $ freshDBVSpace n 
+
+    VecSum _ -> Right $ VProp $ freshDBVSpace n
     VecMin -> Right $ VProp $ freshDBVSpace n
     VecMinL -> Right $ VProp $ freshDBVSpace n
     VecMax -> Right $ VProp $ freshDBVSpace n
@@ -102,7 +102,7 @@ inferIndexSpaceUnOp is n op =
 
     ReverseA -> do
       return $ VPropPair (freshDBVSpace n) (uncurry PropVectorTransform $ freshTransformSpaces n)
-      
+
     ReverseL -> Right $ VPropPair (freshDBVSpace n) (uncurry PropVectorTransform $ freshTransformSpaces n)
     FalsePositions -> Right $ VProp $ freshDBVSpace n
     SelectPos1 _ _ -> Right $ VPropPair (freshDBVSpace n) (uncurry RenameVectorTransform $ freshTransformSpaces n)
@@ -121,7 +121,7 @@ inferIndexSpaceUnOp is n op =
             STNumber   -> T $ NumberDom pis
 
       Right $ VProp $ RenameVectorTransform src dst
-            
+
     ProjectPayload _ -> Right is
     CompExpr1L _ -> Right is
 
@@ -136,9 +136,9 @@ inferIndexSpaceUnOp is n op =
             PosConst _  -> P $ freshSpace n "p"
             PosIdentity -> P pis
       return $ VProp $ DBVSpace dis' pis'
-      
+
     SelectExpr _ -> do
-      ((D dis), (P pis)) <- unp is >>= fromDBV 
+      ((D dis), (P pis)) <- unp is >>= fromDBV
       let dis' = makeSubDomain n "d" dis
           pis' = makeSubDomain n "p" pis
       return $ VProp $ DBVSpace (D dis') (P pis')
@@ -146,29 +146,29 @@ inferIndexSpaceUnOp is n op =
     Singleton -> undefined
 
 
-    R1 -> 
+    R1 ->
       case is of
         VPropPair s1 _ -> Right $ VProp s1
         VPropTriple s1 _ _ -> Right $ VProp s1
         _ -> Left "IndexSpace: Input of R1 is not a tuple"
-    R2 -> 
+    R2 ->
       case is of
         VPropPair _ s2 -> Right $ VProp s2
         VPropTriple _ s2 _ -> Right $ VProp s2
         _ -> Left "IndexSpace: Input of R2 is not a tuple"
-    R3 -> 
+    R3 ->
       case is of
-        VPropTriple s3 _ _ -> Right $ VProp s3
+        VPropTriple _ _ s3 -> Right $ VProp s3
         _ -> Left "IndexSpace: Input of R3 is not a tuple"
 
 inferIndexSpaceBinOp :: VectorProp IndexSpace
                         -> VectorProp IndexSpace
-                        -> AlgNode 
-                        -> BinOp 
+                        -> AlgNode
+                        -> BinOp
                         -> Either String (VectorProp IndexSpace)
-inferIndexSpaceBinOp is1 is2 n op = 
+inferIndexSpaceBinOp is1 is2 n op =
   case op of
-    GroupBy ->  
+    GroupBy ->
       let ddis = D $ freshSpace n "d/d"
           dpis = P $ freshSpace n "d/p"
           vdis = D $ freshSpace n "v/d"
@@ -177,8 +177,8 @@ inferIndexSpaceBinOp is1 is2 n op =
           dbv  = DBVSpace vdis vpis
           pv   = uncurry PropVectorTransform $ freshTransformSpaces n
       in Right $ VPropTriple dv dbv pv
-          
-    SortWith -> 
+
+    SortWith ->
       let ddis = D $ freshSpace n "d/d"
           dpis = P $ freshSpace n "d/p"
           vdis = D $ freshSpace n "v/d"
@@ -201,14 +201,15 @@ inferIndexSpaceBinOp is1 is2 n op =
     DistPrim -> Right $ uncurry VPropPair $ freshValuePropPair n
     DistDesc -> Right $ uncurry VPropPair $ freshValuePropPair n
     DistLift -> Right $ uncurry VPropPair $ freshValuePropPair n
+
     PropRename -> do
       (_, (T tis)) <- unp is1 >>= fromRenameVector
       (_, (P pis)) <- unp is2 >>= fromDBV
       return $ VProp $ DBVSpace (D tis) (P pis)
-      
+
     PropFilter -> Right $ uncurry VPropPair $ freshValueRenamePair n
     PropReorder -> Right $ uncurry VPropPair $ freshValuePropPair n
-    Append -> 
+    Append ->
       let d1sis = S $ freshSpace n "d1/s"
           d1tis = T $ freshSpace n "d1/t"
           d2sis = S $ freshSpace n "d2/s"
@@ -225,7 +226,7 @@ inferIndexSpaceBinOp is1 is2 n op =
     PairA -> Right $ VProp $ DBPSpace $ P $ freshSpace n "p"
     PairL -> Right $ VProp $ freshDBVSpace n
 
-    ZipL -> 
+    ZipL ->
       let d1sis = S $ freshSpace n "d1/s"
           d1tis = T $ freshSpace n "d1/t"
           d2sis = S $ freshSpace n "d2/s"
@@ -237,7 +238,7 @@ inferIndexSpaceBinOp is1 is2 n op =
     -- FIXME d \in p(q1)
     CartProduct -> Right $ VPropTriple (freshDBVSpace n) (freshPropSpace n) (freshPropSpace n)
 
-    ThetaJoin _ -> do 
+    ThetaJoin _ -> do
       ((D dis), (P pis)) <- unp is1 >>= fromDBV
       let -- descr: descriptor of the left input on the first output
           dis'  = D $ makeSubDomain n "d" dis
@@ -245,15 +246,15 @@ inferIndexSpaceBinOp is1 is2 n op =
           dis'' = D $ makeSubDomain n "d" pis
           pis'  = P $ freshSpace n "p"
       Right $ VPropPair (DBVSpace dis' pis') (DBVSpace dis'' pis')
-      
 
-inferIndexSpaceTerOp :: VectorProp IndexSpace 
+
+inferIndexSpaceTerOp :: VectorProp IndexSpace
                         -> VectorProp IndexSpace
-                        -> VectorProp IndexSpace 
-                        -> AlgNode 
-                        -> TerOp 
+                        -> VectorProp IndexSpace
+                        -> AlgNode
+                        -> TerOp
                         -> Either String (VectorProp IndexSpace)
-inferIndexSpaceTerOp _ _ _ n op = 
+inferIndexSpaceTerOp _ _ _ n op =
   case op of
     CombineVec ->
       let d1sis = S $ freshSpace n "d1/s"
