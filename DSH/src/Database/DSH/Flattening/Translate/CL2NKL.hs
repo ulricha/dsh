10be93d8fce68ef42fdf1ebdb8ef9281dd1d0230
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2013-06-12 17:25:59 +0200

    Desguaring of CL comprehensions into NKL expressions.

--- DSH/src/Database/DSH/Flattening/Translate/CL2NKL.hs
+++ DSH/src/Database/DSH/Flattening/Translate/CL2NKL.hs
@@ -1,7 +1,132 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Database.DSH.Flattening.Translate.CL2NKL where
+       
+import           Data.List
+
+import           Database.DSH.Impossible
+       
+import           Database.DSH.Flattening.Common.Data.Expr
+import           Database.DSH.Flattening.Common.Data.Type
+import           Database.DSH.Flattening.Common.Data.Val
+import           Database.DSH.Flattening.Common.Data.Op
 
 import qualified Database.DSH.Flattening.CL.Lang as CL
 import qualified Database.DSH.Flattening.NKL.Data.NKL as NKL
+import qualified Database.DSH.Flattening.CL.Primitives as CP
+       
+-- To transform CL into NKL we have to get rid of comprehensions. However, we
+-- don't want to implement full comprehension desugaring. To avoid it, we
+-- transform via an intermediate step: comprehensions with multiple generators
+-- are transformed into comprehensions with one generator, where the nested
+-- iterations are expressed in the form of cartesian products. The resulting
+-- single-qualifier comprehension should then be easy to desugar (just a map).
+
+prim1 :: CL.Prim1 Type -> NKL.Prim1 Type
+prim1 = undefined
+
+prim2 :: CL.Prim2 Type -> NKL.Prim2 Type
+prim2 (CL.Prim2 o t) = undefined
+
+expr :: CL.Expr -> NKL.Expr
+expr (CL.Table t s cs ks) = NKL.Table t s cs ks
+expr (CL.App t e1 e2)     = NKL.App t (expr e1) (expr e2)
+expr (CL.AppE1 t p e)     = NKL.AppE1 t (prim1 p) (expr e)
+expr (CL.AppE2 t p e1 e2) = NKL.AppE2 t (prim2 p) (expr e1) (expr e2)
+expr (CL.BinOp t o e1 e2) = NKL.BinOp t o (expr e1) (expr e2)
+expr (CL.Lam t v e)       = NKL.Lam t v (expr e)
+expr (CL.If t c th el)    = NKL.If t (expr c) (expr th) (expr el)
+expr (CL.Const t v)       = NKL.Const t v
+expr (CL.Var t v)         = NKL.Var t v
+expr (CL.Comp t e qs)     = desugar t e qs
+
+-- | Desugar comprehensions into NKL expressions
+desugar :: Type -> CL.Expr -> [CL.Qualifier] -> NKL.Expr
+desugar t e qs =
+  -- We reduce a comprehension with multiple qualifiers to a comprehension with
+  -- one qualifier, which we can then handle easily.
+  case productify e qs of 
+    (e, CL.BindQ x xs) -> expr $ CP.map e xs
+    (e, CL.GuardQ p)   -> expr $ CL.If t p (CL.BinOp t Cons e empty) empty
+    
+  where empty = CL.Const t (ListV [])
+
+-- | Turn multiple qualifiers into one qualifier using cartesian products and
+-- filters to express nested iterations and predicates.
+productify :: CL.Expr -> [CL.Qualifier] -> (CL.Expr, CL.Qualifier)
+productify e []                                 = $impossible
+productify e [q]                                = (e, q)
+           
+-- [ e | x <- xs, y <- ys, qs ] = 
+-- [ e[fst x/x][snd x/y] | x <- cartProd xs ys, qs[fst x/x][snd x/y] ]
+productify e ((CL.BindQ x xs) : (CL.BindQ y ys) : qs) = 
+  productify e' (q' : qs')
+  
+  where e'  = tuplify (x, xt) (y, yt) e
+        qs' = tuplifyQuals (x, xt) (y, yt) qs
+        q'  = CL.BindQ x (CL.AppE2 undefined (CL.Prim2 CL.CartProduct undefined) xs ys)
+        xt  = elemT $ typeOf xs
+        yt  = elemT $ typeOf ys 
+
+-- [ e | x <- xs, p, qs ] = [ e | x <- filter (\x -> p) xs, qs ]
+productify e ((CL.BindQ x xs) : (CL.GuardQ p)   : qs) = 
+  productify e (q' : qs)
+
+  where q'  = CL.BindQ x (CL.AppE2 (listT xt) (CL.Prim2 CL.Filter ft) (CL.Lam (xt .-> boolT) x p) xs)
+        ft  = (xt .-> boolT) .-> (xst .-> xst)
+        xst = typeOf xs
+        xt  = elemT xst
+           
+-- [ e | p1, p2, qs ] = [ e | p1 && p2, qs ]
+productify e ((CL.GuardQ p1)  : (CL.GuardQ p2)  : qs) = 
+  productify e (q' : qs)
+
+  where q' = CL.GuardQ $ CL.BinOp boolT Conj p1 p2
+           
+-- [ e | p1, x <- xs, qs ] = [ e | x <- filter (\x -> p) xs, qs ]
+productify e ((CL.GuardQ p)   : (CL.BindQ x xs) : qs) = 
+  productify e ((CL.GuardQ p) : (CL.BindQ x xs) : qs)
+
+-- | Substitution: subst v r e ~ e[v/r]
+subst :: Ident -> CL.Expr -> CL.Expr -> CL.Expr
+subst _ _ table@(CL.Table _ _ _ _) = table
+subst v r (CL.App t e1 e2)         = CL.App t (subst v r e1) (subst v r e2)
+subst v r (CL.AppE1 t p e)         = CL.AppE1 t p (subst v r e)
+subst v r (CL.AppE2 t p e1 e2)     = CL.AppE2 t p (subst v r e1) (subst v r e2)
+subst v r (CL.BinOp t o e1 e2)     = CL.BinOp t o (subst v r e1) (subst v r e2)
+-- FIXME for the moment, we assume that all lambda variables are unique
+-- and we don't need to check for name capturing/do alpha-conversion.
+subst v r lam@(CL.Lam t v' e)      = if v' == v
+                                     then lam
+                                     else CL.Lam t v' (subst v r e)
+subst _ _ c@(CL.Const _ _)         = c
+subst v r var@(CL.Var _ v')        = if v == v' then r else var
+subst v r (CL.If t c thenE elseE)  = CL.If t (subst v r c) (subst v r thenE) (subst v r elseE)
+
+-- | Substitution on a list of qualifiers
+substQuals :: Ident -> CL.Expr -> [CL.Qualifier] -> [CL.Qualifier]
+substQuals v r ((CL.GuardQ g) : qs)               = (CL.GuardQ (subst v r g)) : (substQuals v r qs)
+substQuals v r ((CL.BindQ v' s) : qs) | v == v'   = (CL.BindQ v' (subst v r s)) : qs
+substQuals v r ((CL.BindQ v' s) : qs) | otherwise = (CL.BindQ v' (subst v r s)) : (substQuals v r qs)
+substQuals _ _ []                                 = []
+
+-- tuplify v1 v2 e = e[v1/fst v1][v2/snd v1]
+tuplify :: (Ident, Type) -> (Ident, Type) -> CL.Expr -> CL.Expr
+tuplify (v1, t1) (v2, t2) e = subst v2 v2Rep $ subst v1 v1Rep e
+
+  where (v1Rep, v2Rep) = tupleVars (v1, t1) (v2, t2)
+
+tuplifyQuals :: (Ident, Type) -> (Ident, Type) -> [CL.Qualifier] -> [CL.Qualifier]
+tuplifyQuals (v1, t1) (v2, t2) qs = substQuals v2 v2Rep $ substQuals v1 v1Rep qs
+
+  where (v1Rep, v2Rep) = tupleVars (v1, t1) (v2, t2)
+
+tupleVars :: (Ident, Type) -> (Ident, Type) -> (CL.Expr, CL.Expr)
+tupleVars (v1, t1) (v2, t2) = (v1Rep, v2Rep)
+  where v1'    = CL.Var pt v1
+        pt     = pairT t1 t2
+        v1Rep  = CL.AppE1 t1 (CL.Prim1 CL.Fst (pt .-> t1)) v1'
+        v2Rep  = CL.AppE1 t2 (CL.Prim1 CL.Snd (pt .-> t2)) v1'
 
 -- | Express comprehensions in NKL iteration constructs map and concatMap.
 desugarComprehensions :: CL.Expr -> NKL.Expr
