4f09b2cd46fd242d0311f688f59abddf6823d4a4
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2013-09-16 09:12:10 +0200

    Deactivated executables and tests for now.

--- DSH/src/Database/DSH/Translate/CL2NKL.hs
+++ DSH/src/Database/DSH/Translate/CL2NKL.hs
@@ -69,6 +69,7 @@ prim2 (CL.Prim2 o t) = NKL.Prim2 o' t
               CL.EquiJoin e1 e2 -> NKL.EquiJoin e1 e2
               CL.NestJoin e1 e2 -> NKL.NestJoin e1 e2
               CL.SemiJoin e1 e2 -> NKL.SemiJoin e1 e2
+              CL.AntiJoin e1 e2 -> NKL.AntiJoin e1 e2
               CL.ConcatMap      -> $impossible
 
 expr :: CL.Expr -> NKL.Expr
