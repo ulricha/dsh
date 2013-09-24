46013108ca220f3988604efc34ce21ebf4c8bbe3
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2013-09-24 13:10:49 +0200

    Additional rewrites for universal quantification/antijoin introduction.

--- DSH/src/Database/DSH/CL/Opt/FlatJoin.hs
+++ DSH/src/Database/DSH/CL/Opt/FlatJoin.hs
@@ -12,9 +12,10 @@ module Database.DSH.CL.Opt.FlatJoin
   
 import Control.Arrow
 
-import Database.DSH.CL.Lang
 import Database.DSH.CL.Kure
+import Database.DSH.CL.Lang
 import Database.DSH.CL.Opt.Aux
+import qualified Database.DSH.CL.Primitives as P
 
 --------------------------------------------------------------------------------
 -- Introduce simple equi joins
@@ -138,6 +139,9 @@ semijoinR = do
 --------------------------------------------------------------------------------
 -- Introduce anti joins (universal quantification)
 
+--------------------------------------------------------------------------------
+-- Basic antijoin pattern
+
 -- | Construct an antijoin qualifier given a predicate and two generators. Note
 -- that the splitJoinPred call implicitly checks that only x and y occur free in
 -- the predicate and no further correlation takes place.
@@ -149,31 +153,113 @@ mkantijoinT joinPred x y xs ys = do
         yst = typeOf ys
         jt  = xst .-> yst .-> xst
 
-    -- => [ ... | ..., x <- xs semijoin(p1, p2) ys, ... ]
+    -- => [ ... | ..., x <- xs antijoin(p1, p2) ys, ... ]
     return $ BindQ x (AppE2 xst (Prim2 (AntiJoin leftExpr rightExpr) jt) xs ys)
 
--- | Match a NOT IN antijoin pattern in the middle of a qualifier list
-notinR :: RewriteC (NL Qual)
-notinR = do
-    -- [ ... | ..., x <- xs, and [ True | y <- ys, not p ], ... ]
+-- | Match the basic antijoin pattern in the middle of a qualifier list. This is
+-- essentially the operator definition, generalized to multiple qualifiers and
+-- an arbitrary comprehension head:
+-- [ f x | qs, x <- xs, and [ not (q y) | y <- ys ], qs' ]
+-- => [ f x | qs, x <- xs antijoin(q) ys, qs' ]
+basicAntiJoinR :: RewriteC (NL Qual)
+basicAntiJoinR = do
+    -- [ ... | ..., x <- xs, and [ not p | y <- ys ], ... ]
     BindQ x xs :* GuardQ (AppE1 _ (Prim1 And _) 
-                                  (Comp _ (Lit _ (BoolV True)) 
-                                          (BindQ y ys :* (S (GuardQ (AppE1 _ (Prim1 Not _) p)))))) :* qs <- idR
+                                  (Comp _ (AppE1 _ (Prim1 Not _) p)
+                                          (S (BindQ y ys))))  :* qs <- idR
     q' <- mkantijoinT p x y xs ys
     return $ q' :* qs
 
 -- | Match a NOT IN antijoin pattern at the end of a list
-notinEndR :: RewriteC (NL Qual)
-notinEndR = do
+basicAntiJoinEndR :: RewriteC (NL Qual)
+basicAntiJoinEndR = do
     -- [ ... | ..., x <- xs, and [ True | y <- ys, not p ] ]
-    BindQ x xs :* (S (GuardQ (AppE1 _ (Prim1 And _) 
-                                      (Comp _ (Lit _ (BoolV True)) 
-                                              ((BindQ y ys) :* (S (GuardQ (AppE1 _ (Prim1 Not _) p)))))))) <- idR
+    BindQ x xs :* S (GuardQ (AppE1 _ (Prim1 And _) 
+                                     (Comp _ (AppE1 _ (Prim1 Not _) p)
+                                             (S (BindQ y ys))))) <- idR
     q' <- mkantijoinT p x y xs ys
     return (S q')
+
+--------------------------------------------------------------------------------
+-- Doubly Negated existential quantifier (NOT EXISTS)
+
+notinR :: RewriteC (NL Qual)
+notinR = do
+    BindQ x xs :* 
+        (GuardQ (AppE1 _ (Prim1 Not _)
+                         (AppE1 _ (Prim1 Or _)
+                                  (Comp _ q (BindQ y ys :* (S (GuardQ p))))))) :* qs <- idR
+                                  
+    -- 
     
+    q' <- mkClass15AntiJoinT x xs y ys p q
+    return $ q' :* qs
+
+notinEndR :: RewriteC (NL Qual)
+notinEndR = do
+    BindQ x xs :* 
+        (S (GuardQ (AppE1 _ (Prim1 Not _)
+                            (AppE1 _ (Prim1 Or _)
+                                     (Comp _ q (BindQ y ys :* (S (GuardQ p)))))))) <- idR
+                                  
+    -- 
+    
+    q' <- mkClass15AntiJoinT x xs y ys p q
+    return (S q')
+
+--------------------------------------------------------------------------------
+-- Universal quantification with range predicates
+    
+-- | Turn universal quantification with range and quantifier predicates into an
+-- antijoin. We use the classification of queries in Claussen et al.: Optimizing
+-- Queries with Universal Quantification in Object-Oriented and
+-- Object-Relational Databases (VLDB 1995).
+universalR :: RewriteC (NL Qual)
+universalR = do
+    -- [ ... | ..., x <- xs, and [ q y | y <- ys, p x y ], ... ]
+    BindQ x xs :* (GuardQ (AppE1 _ (Prim1 And _)
+                                   (Comp _ q ((BindQ y ys) :* (S (GuardQ p)))))) :* qs<- idR
+                                   
+    q' <- mkClass15AntiJoinT x xs y ys p q
+    return $ q' :* qs
+    
+universalEndR :: RewriteC (NL Qual)
+universalEndR = do
+    -- [ ... | ..., x <- xs, and [ q y | y <- ys, p x y ] ]
+    BindQ x xs :* (S (GuardQ (AppE1 _ (Prim1 And _)
+                                      (Comp _ q ((BindQ y ys) :* (S (GuardQ p))))))) <- idR
+                                      
+    q' <- mkClass15AntiJoinT x xs y ys p q
+    return (S q')
+
+-- This rewrite implements plan 14 for Query Class 15 in Claussen et al.,
+-- Optimizing Queries with Universal Quantification... (VLDB, 1995).  Class 15
+-- contains queries in which the range predicate ranges over both relations,
+-- i.e. x and y occur free. The quantifier predicate on the other hand ranges
+-- only over the inner relation.
+mkClass15AntiJoinT :: Ident -> Expr -> Ident -> Expr -> Expr -> Expr -> TranslateC (NL Qual) Qual
+mkClass15AntiJoinT x xs y ys p q = do
+    -- Check that the quantifier predicate only ranges over the inner relation
+    guardM $ freeVars q == [y]
+    
+    -- The range predicate must have the form of a join predicate. This
+    -- implicitly checks that the range predicate ranges over x and y.
+    (p1, p2) <- constT (return p) >>> splitJoinPredT x y
+    
+    let yst = typeOf ys
+        yt  = elemT yst
+    
+    -- => [ ... | ..., x <- xs antijoin(p1, p2) [ y | y <- ys, not q ], ...]
+    let q' = BindQ x (P.antijoin xs (Comp yst (Var yt y) (BindQ y ys :* (S (GuardQ (P.not q))))) p1 p2)
+    return q'
+
 universalQualsR :: RewriteC (NL Qual)
-universalQualsR = anytdR $ repeatR (notinR <+ notinEndR)
+universalQualsR = anytdR $ basicAntiJoinR 
+                           <+ basicAntiJoinEndR 
+                           <+ notinR
+                           <+ notinEndR
+                           <+ universalR 
+                           <+ universalEndR
 
 antijoinR :: RewriteC CL
 antijoinR = do
