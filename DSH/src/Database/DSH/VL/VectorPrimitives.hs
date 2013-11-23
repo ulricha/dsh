1fecc7674911174ee217c563db255ad838abd6d4
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2013-11-23 03:51:48 +0100

    Comments on VL operators that can be simplified.

--- DSH/src/Database/DSH/VL/VectorPrimitives.hs
+++ DSH/src/Database/DSH/VL/VectorPrimitives.hs
@@ -11,23 +11,38 @@ import Database.Algebra.Pathfinder()
 
 class VectorAlgebra a where
   singletonDescr :: GraphM r a DBV
+  
+  -- FIXME rename to litAtom
   constructLiteralValue :: [VLType] -> [VLVal] -> GraphM r a DBP
+  -- FIXME rename to litTable
   constructLiteralTable :: [VLType] -> [[VLVal]] -> GraphM r a DBV
   tableRef :: String -> [TypedColumn] -> [Key] -> GraphM r a DBV
 
+  -- FIXME rename to distinct
   unique :: DBV -> GraphM r a DBV
   uniqueL :: DBV -> GraphM r a DBV
 
   number :: DBV -> GraphM r a DBV
   numberL :: DBV -> GraphM r a DBV  
 
+  -- FIXME replace with project
   notPrim :: DBP -> GraphM r a DBP
+  -- FIXME replace with projectL
   notVec :: DBV -> GraphM r a DBV
-  lengthA :: DBV -> GraphM r a DBP
+
   descToRename :: DBV -> GraphM r a RenameVector
   segment :: DBV -> GraphM r a DBV
   unsegment :: DBV -> GraphM r a DBV
+  
+  -- FIXME combine into generic aggregate operator
+  -- although: backend implementation can be quite different.
   vecSum :: VLType -> DBV -> GraphM r a DBP
+  vecSumLift :: DBV -> DBV -> GraphM r a DBV
+  -- Avg is unsafe! Empty lists will disappear, as average does not have a
+  -- default value (in contrast to sum).
+  vecAvgLift :: DBV -> DBV -> GraphM r a DBV
+  lengthSeg :: DBV -> DBV -> GraphM r a DBV
+  lengthA :: DBV -> GraphM r a DBP
   -- Avg is unsafe! Empty lists will disappear, as average does not have a
   -- default value (in contrast to sum).
   vecAvg :: DBV -> GraphM r a DBP
@@ -35,21 +50,36 @@ class VectorAlgebra a where
   vecMinLift :: DBV -> GraphM r a DBV
   vecMax :: DBV -> GraphM r a DBP
   vecMaxLift :: DBV -> GraphM r a DBV
+
   selectPos1 :: DBV -> VecCompOp -> Nat -> GraphM r a (DBV, RenameVector)
   selectPos1Lift :: DBV -> VecCompOp -> Nat -> GraphM r a (DBV, RenameVector)
+
   projectL :: DBV -> [DBCol] -> GraphM r a DBV
   projectA :: DBP -> [DBCol] -> GraphM r a DBP
+  
+  {-
+  projectL :: [Proj] -> DBV -> GraphM r a DBV
+  project  :: [Proj] -> DBP -> GraphM r a DBP
+  -}
   
   -- FIXME this should be a generic cast operator
   integerToDoubleA :: DBP -> GraphM r a DBP
   integerToDoubleL :: DBV -> GraphM r a DBV
+
   reverseA :: DBV -> GraphM r a (DBV, PropVector)
   reverseL :: DBV -> GraphM r a (DBV, PropVector)
   falsePositions :: DBV -> GraphM r a DBV
+
   selectExpr :: Expr1 -> DBV -> GraphM r a DBV
+
+  -- FIXME eliminate
   projectRename :: ISTransProj -> ISTransProj -> DBV -> GraphM r a RenameVector
+  -- FIXME eliminate
   projectAdmin :: DescrProj -> PosProj -> DBV -> GraphM r a DBV
+
   projectPayload :: [PayloadProj] -> DBV -> GraphM r a DBV
+  
+  -- FIXME replace by projectL
   compExpr1 :: Expr1 -> DBV -> GraphM r a DBV
 
   groupByKey :: DBV -> DBV -> GraphM r a (DBV, DBV, PropVector)
@@ -59,8 +89,8 @@ class VectorAlgebra a where
   -- argument. The result is a flat vector, since all groups are reduced via
   -- aggregation.
   vecAggr :: [DBCol] -> [AggrFun] -> DBV -> GraphM r a DBV
+
   sortWith :: DBV -> DBV -> GraphM r a (DBV, PropVector)
-  lengthSeg :: DBV -> DBV -> GraphM r a DBV
   distPrim :: DBP -> DBV -> GraphM r a (DBV, PropVector)
   distDesc :: DBV -> DBV -> GraphM r a (DBV, PropVector)
   distLift :: DBV -> DBV -> GraphM r a (DBV, PropVector)
@@ -72,17 +102,19 @@ class VectorAlgebra a where
   propReorder :: PropVector -> DBV -> GraphM r a (DBV, PropVector)
   append :: DBV -> DBV -> GraphM r a (DBV, RenameVector, RenameVector)
   restrictVec :: DBV -> DBV -> GraphM r a (DBV, RenameVector)
+  
+  -- FIXME better name
   compExpr2 :: Expr2 -> DBP -> DBP -> GraphM r a DBP
   compExpr2L :: Expr2 -> DBV -> DBV -> GraphM r a DBV
-  vecSumLift :: DBV -> DBV -> GraphM r a DBV
-  -- Avg is unsafe! Empty lists will disappear, as average does not have a
-  -- default value (in contrast to sum).
-  vecAvgLift :: DBV -> DBV -> GraphM r a DBV
+
   selectPos :: DBV -> VecCompOp -> DBP -> GraphM r a (DBV, RenameVector)
   selectPosLift :: DBV -> VecCompOp -> DBV -> GraphM r a (DBV, RenameVector)
+
   pairA :: DBP -> DBP -> GraphM r a DBP
   pairL :: DBV -> DBV -> GraphM r a DBV
+
   zipL :: DBV -> DBV -> GraphM r a (DBV, RenameVector, RenameVector)
+
   cartProduct :: DBV -> DBV -> GraphM r a (DBV, PropVector, PropVector)
   cartProductL :: DBV -> DBV -> GraphM r a (DBV, PropVector, PropVector)
 
