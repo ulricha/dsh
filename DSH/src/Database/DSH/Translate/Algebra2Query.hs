e5f132b9c408ffec92761ee7fbb5121e5709701c
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2013-12-03 13:50:32 +0100

    Removed pathfinder interaction, i.e. SQL code generation. This still leaves in the generation of TableAlgebra XML plans.

--- DSH/src/Database/DSH/Translate/Algebra2Query.hs
+++ DSH/src/Database/DSH/Translate/Algebra2Query.hs
@@ -1,15 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Database.DSH.Translate.Algebra2Query (generateX100Query, generatePFXML, generateSQL) where
+module Database.DSH.Translate.Algebra2Query (generateX100Query, generatePFXML) where
 
 import           Text.PrettyPrint.HughesPJ
 
-import           Text.XML.HaXml                                (children, deep, tag, xmlParse)
-import qualified Text.XML.HaXml                                as X
-
 import qualified Data.IntMap                                   as IM
 import qualified Data.Map                                      as M
-import           Data.Maybe
 
 import           Database.Algebra.Dag
 import           Database.Algebra.Dag.Common
@@ -19,13 +15,8 @@ import           Database.Algebra.X100.Data
 import           Database.Algebra.X100.Render
 
 import           Database.DSH.Common.Data.QueryPlan hiding (mkQueryPlan)
-import           Database.DSH.Impossible
 import           Database.DSH.VL.Data.DBVector
-import qualified Database.DSH.VL.Data.Query         as Ext
 import qualified Database.DSH.VL.Data.Query         as Q
-import           Database.Pathfinder
-
-import           System.IO.Unsafe
 
 generateX100Query :: QueryPlan X100Algebra -> Q.Query Q.X100
 generateX100Query x100Plan = convertQuery $ queryShape x100Plan
@@ -67,51 +58,3 @@ generatePFXML pfPlan = convertQuery $ queryShape pfPlan
                         runXML False M.empty IM.empty $
                         mkQueryPlan Nothing (xmlElem "property") $
                         runXML True nodeTable (queryTags pfPlan) $ serializeAlgebra cs n
-
-generateSQL :: Ext.Query Ext.XML -> Ext.Query Ext.SQL
-generateSQL (Ext.ValueVector (Ext.XML i r') lyt) = Ext.ValueVector ((\(q, s) -> Ext.SQL i s q) $ translate r') $ translateLayout lyt
-generateSQL (Ext.PrimVal (Ext.XML i r') lyt) = Ext.PrimVal ((\(q, s) -> Ext.SQL i s q) $ translate r') $ translateLayout lyt
-
-translateLayout :: Ext.Layout Ext.XML -> Ext.Layout Ext.SQL
-translateLayout (Ext.InColumn i) = Ext.InColumn i
-translateLayout (Ext.Nest (Ext.XML i r') lyt) = Ext.Nest ((\(q,s) -> Ext.SQL i s q) $ translate r') (translateLayout lyt)
-translateLayout (Ext.Pair l1 l2) = Ext.Pair (translateLayout l1) (translateLayout l2)
-
-translate :: String -> (String, Ext.Schema)
-translate xml = let r' = unsafePerformIO $ pathfinder xml "" OutputSql
-                 in case r' of
-                     (Right sql) -> extractSQL sql
-                     (Left err) -> error $ "Pathfinder compilation for input: \n"
-                                             ++ xml ++ "\n failed with error: \n"
-                                             ++ err
-
-extractSQL :: String -> (String, Ext.Schema)
-extractSQL xml = let (Document _ _ r _) = xmlParse "query" xml
-                     q = extractCData $ head $ concatMap children $ (deep $ tag "query") (CElem r undefined)
-                     s = toSchemeInf $ map process $ concatMap (\x -> deep (tag "column") x) $ deep (tag "schema") (CElem r undefined)
-                  in (q, s)
-
-
-process :: Content i -> (String, Maybe Int)
-process (CElem (X.Elem _ attrs _) _) = let name = fromJust $ fmap attrToString $ lookup (X.N "name") attrs
-                                           pos = fmap attrToInt $ lookup (X.N "position") attrs
-                                        in (name, pos)
-process _ = $impossible
-
-toSchemeInf :: [(String, Maybe Int)] -> Ext.Schema
-toSchemeInf results = let iterName = fst $ head $ filter (\(_, p) -> isNothing p) results
-                          cols = map (\(n, v) -> (n, fromJust v)) $ filter (\(_, p) -> isJust p) results
-                       in (iterName, cols)
-
-extractCData :: Content i -> String
-extractCData (CString _ d _) = d
-extractCData _               = error "Not CDATA"
-
-
-attrToInt :: AttValue -> Int
-attrToInt (AttValue [(Left i)]) = read i
-attrToInt _ = $impossible
-
-attrToString :: AttValue -> String
-attrToString (AttValue [(Left i)]) = i
-attrToString _ = $impossible
