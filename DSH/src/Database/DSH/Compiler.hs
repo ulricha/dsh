ef0ab469919845483dd09c592376fc0ae726b33d
Alexander Ulrich <alex@etc-network.de>
2013-12-30 17:56:29 +0100

    Somehow the X100 stuff sneaked in again.

--- DSH/src/Database/DSH/Compiler.hs
+++ DSH/src/Database/DSH/Compiler.hs
@@ -3,18 +3,11 @@
 -- | This module provides the flattening implementation of DSH.
 module Database.DSH.Compiler
   ( -- * Running queries via the Flattening backend
-    fromQX100
     -- * Debug functions
-  , debugNKL
+    debugNKL
   , debugFKL
-  , debugX100
-  , debugCLX100
-  , debugNKLX100
-  , debugFKLX100
   , debugVL
-  , debugX100VL
   , debugTA
-  , dumpVLMem
   ) where
 
 import           GHC.Exts
@@ -26,9 +19,6 @@ import           Database.DSH.ExecuteFlattening
 import           Database.DSH.Internals
 import           Database.HDBC
 import qualified Database.HDBC                                   as H
-
-import           Database.X100Client                             hiding (X100, tableName)
-import qualified Database.X100Client                             as X
 
 import           Database.Algebra.Dag
 
@@ -60,24 +50,6 @@ import           Data.Convertible                                ()
 
 -- Different versions of the flattening compiler pipeline
 
-nkl2X100Alg :: CL.Expr -> (Q.Query Q.X100, T.Type)
-nkl2X100Alg e = let q = desugarComprehensions e
-                        |> flatten
-                        |> specializeVectorOps
-                        |> optimizeVLDefault
-                        |> implementVectorOpsX100
-                        |> generateX100Query
-                    t = T.typeOf e
-                in (q, t)
-
-nkl2X100File :: String -> CL.Expr -> IO ()
-nkl2X100File prefix e = desugarComprehensions e
-                        |> flatten
-                        |> specializeVectorOps
-                        |> optimizeVLDefault
-                        |> implementVectorOpsX100
-                        |> (exportX100Plan prefix)
-
 nkl2TAFile :: String -> CL.Expr -> IO ()
 nkl2TAFile prefix e = desugarComprehensions e
                         |> flatten
@@ -97,42 +69,15 @@ nkl2VLFile prefix e = desugarComprehensions e
 
 -- Functions for executing and debugging DSH queries via the Flattening backend
 
--- | Compile a DSH query to X100 algebra and run it on the X100 server given by 'c'.
-fromQX100 :: QA a => X100Info -> Q a -> IO a
-fromQX100 c (Q a) =  do
-                  (q, _) <- nkl2X100Alg <$> CLOpt.opt <$> toComprehensions (getX100TableInfo c) a
-                  frExp <$> (executeX100Query c $ X100 q)
-                  
--- | Debugging function: return the CL (Comprehension Language) representation of a
--- query (X100 version)
-debugCLX100 :: QA a => X100Info -> Q a -> IO String
-debugCLX100 c (Q e) = show <$> CLOpt.opt <$> toComprehensions (getX100TableInfo c) e
-
 -- | Debugging function: return the NKL (Nested Kernel Language) representation of a
 -- query (SQL version)
 debugNKL :: (QA a, IConnection conn) => conn -> Q a -> IO String
 debugNKL c (Q e) = show <$> desugarComprehensions <$> CLOpt.opt <$> toComprehensions (getTableInfo c) e
 
--- | Debugging function: return the NKL (Nested Kernel Language) representation of a
--- query (X100 version)
-debugNKLX100 :: QA a => X100Info -> Q a -> IO String
-debugNKLX100 c (Q e) = show <$> desugarComprehensions <$> CLOpt.opt <$> toComprehensions (getX100TableInfo c) e
-
 -- | Debugging function: return the FKL (Flat Kernel Language) representation of a
 -- query (SQL version)
 debugFKL :: (QA a, IConnection conn) => conn -> Q a -> IO String
 debugFKL c (Q e) = show <$> flatten <$> desugarComprehensions <$> toComprehensions (getTableInfo c) e
-
--- | Debugging function: return the FKL (Flat Kernel Language) representation of a
--- query (X100 version)
-debugFKLX100 :: QA a => X100Info -> Q a -> IO String
-debugFKLX100 c (Q e) = show <$> flatten <$> desugarComprehensions <$> toComprehensions (getX100TableInfo c) e
-
--- | Debugging function: dump the X100 plan (DAG) to a file.
-debugX100 :: QA a => String -> X100Info -> Q a -> IO ()
-debugX100 prefix c (Q e) = do
-              e' <- CLOpt.opt <$> toComprehensions (getX100TableInfo c) e
-              nkl2X100File prefix e'
 
 -- | Debugging function: dump the table algebra plan (JSON) to a file.
 debugTA :: (QA a, IConnection conn) => String -> conn -> Q a -> IO ()
@@ -145,22 +90,6 @@ debugVL :: (QA a, IConnection conn) => String -> conn -> Q a -> IO ()
 debugVL prefix c (Q e) = do
   e' <- CLOpt.opt <$> toComprehensions (getTableInfo c) e
   nkl2VLFile prefix e'
-
--- | Debugging function: dump the VL query plan (DAG) for a query to a file (X100 version).
-debugX100VL :: QA a => String -> X100Info -> Q a -> IO ()
-debugX100VL prefix c (Q e) = do
-  e' <- CLOpt.opt <$> toComprehensions (getX100TableInfo c) e
-  nkl2VLFile prefix e'
-
--- | Dump a VL plan in the JSON format expected by the in-memory implementation (Tobias Müller)
-dumpVLMem :: QA a => FilePath -> X100Info -> Q a -> IO ()
-dumpVLMem f c (Q q) = do
-  cl <- toComprehensions (getX100TableInfo c) q
-  let plan = desugarComprehensions cl
-             |> flatten
-             |> specializeVectorOps
-      json = unpack $ encode (queryShape plan, M.toList $ nodeMap $ queryDag plan)
-  writeFile f json
 
 -- | Retrieve through the given database connection information on the
 -- table (columns with their types) which name is given as the second
@@ -188,38 +117,3 @@ getTableInfo conn tableName = do
             T.IntT    -> L.elem dbT [SqlSmallIntT, SqlIntegerT, SqlTinyIntT, SqlBigIntT, SqlNumericT]
             T.DoubleT -> L.elem dbT [SqlDecimalT, SqlRealT, SqlFloatT, SqlDoubleT]
             t         -> error $ printf "Unsupported column type %s for table %s" (show t) (show tableName)
-
-getX100TableInfo :: X100Info -> String -> IO [(String, String, (T.Type -> Bool))]
-getX100TableInfo c n = do
-    t <- X.describeTable' c n
-    return [ (colName col, show lType, compatibleType lType)
-           | col <- sortWith colName $ columns t
-           , let lType = logicalType col
-           ]
-  where
-    compatibleType :: LType -> T.Type -> Bool
-    compatibleType lt t =
-        case lt of
-            LBool       -> t == T.BoolT || t == T.UnitT
-            LInt1       -> t == T.IntT  || t == T.UnitT
-            LUInt1      -> t == T.IntT  || t == T.UnitT
-            LInt2       -> t == T.IntT  || t == T.UnitT
-            LUInt2      -> t == T.IntT  || t == T.UnitT
-            LInt4       -> t == T.IntT  || t == T.UnitT
-            LUInt4      -> t == T.IntT  || t == T.UnitT
-            LInt8       -> t == T.IntT  || t == T.UnitT
-            LUInt8      -> t == T.IntT  || t == T.UnitT
-            LInt16      -> t == T.IntT  || t == T.UnitT
-            LUIDX       -> t == T.NatT  || t == T.UnitT
-            LDec        -> t == T.DoubleT
-            LFlt4       -> t == T.DoubleT
-            LFlt8       -> t == T.DoubleT
-            LMoney      -> t == T.DoubleT
-            LChar       -> t == T.StringT
-            LVChar      -> t == T.StringT
-            LDate       -> t == T.IntT
-            LTime       -> t == T.IntT
-            LTimeStamp  -> t == T.IntT
-            LIntervalDS -> t == T.IntT
-            LIntervalYM -> t == T.IntT
-            LUnknown s  -> error $ "Unknown DB type" ++ show s
--- DSH/src/Database/DSH/ExecuteFlattening.hs
+++ DSH/src/Database/DSH/ExecuteFlattening.hs
@@ -7,12 +7,10 @@
 {-# LANGUAGE TransformListComp     #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Database.DSH.ExecuteFlattening(executeSQLQuery, executeX100Query, SQL(..), X100(..)) where
+module Database.DSH.ExecuteFlattening(executeSQLQuery, SQL(..)) where
 
 import           Database.DSH.Impossible
 import           Database.DSH.Internals
-
-import           Database.X100Client                   hiding (X100)
 
 import           Database.HDBC
 
@@ -32,8 +30,6 @@ import qualified Database.DSH.VL.Data.Query as Q
 
 data SQL a = SQL (Q.Query Q.SQL)
 
-data X100 a = X100 (Q.Query Q.X100)
-
 textToChar :: Exp Text -> Exp Char
 textToChar (TextE t) = CharE (Txt.head t)
 textToChar _               = error $ "textToChar: Not a char value"
@@ -47,31 +43,6 @@ executeSQLQuery c (SQL q) = let et = reify (undefined :: a)
                                  _         -> do
                                                 n <- makeNormSQLPrim c q et
                                                 return $ n
-
-executeX100Query :: forall a. (Reify a) => X100Info -> X100 a -> IO (Exp a)
-executeX100Query c (X100 q) = let et = reify (undefined :: a)
-                                  in case et of
-                                        (ListT _) -> do
-                                                      n <- makeNormX100 c q et
-                                                      return $ concatN $ reverse $ map snd n
-                                        _ -> do
-                                                n <- makeNormX100Prim c q et
-                                                return n
-
-constructVector :: X100Info -> Q.Layout Q.X100 -> [(Int, [(Int, [X100Data])])] -> Type [a] -> IO [(Int, Exp [a])]
-constructVector _ (Q.InColumn i) parted t = do
-                                            return $ normaliseX100List t i parted
-constructVector c (Q.Nest v lyt) parted t@(ListT _) =
-                                        case t of
-                                            (ListT t1@(ListT _)) -> do
-                                                            inner <- makeNormX100 c (Q.ValueVector v lyt) t1
-                                                            return $ constructDescriptor t (map (\(i, p) -> (i, map fst p)) parted) inner
-                                            _ -> error "constructVector: Not a nested list"
-constructVector c (Q.Pair p1 p2) parted (ListT (PairT t1 t2)) = do
-                                                                    v1 <- constructVector c p1 parted $ ListT t1
-                                                                    v2 <- constructVector c p2 parted $ ListT t2
-                                                                    return $ makeTuple v1 v2
-constructVector _ v _ t = error $ "result " ++ show v ++ " with type: " ++ show t
 
 constructVectorSQL :: IConnection conn => conn -> Q.Layout Q.SQL -> [(String, Int)] -> [(Int, [(Int, [SqlValue])])] -> Type [a] -> IO [(Int, Exp [a])]
 constructVectorSQL _ (Q.InColumn i) pos parted t = do
@@ -98,21 +69,6 @@ makeTuple _              _                          = $impossible
 zipNorm :: Exp [a] -> Exp [b] -> Exp [(a, b)]
 zipNorm (ListE es1) (ListE es2) = ListE [PairE e1 e2 | (e1, e2) <- zip es1 es2]
 zipNorm _ _ = error "zipNorm: Cannot zip"
-
-makeNormX100 :: X100Info -> Q.Query Q.X100 -> Type [a] -> IO [(Int, Exp [a])]
-makeNormX100 c (Q.ValueVector (Q.X100 _ q) p) t = do
-                                                   (X100Res _ res) <- doX100Query c q
-                                                   let parted = partByIterX100 res
-                                                   constructVector c p parted t
-makeNormX100 _ _ _ = error "makeNormX100: Not a list value vector"
-
-makeNormX100Prim :: (Reify a) => X100Info -> Q.Query Q.X100 -> Type a -> IO (Exp a)
-makeNormX100Prim c (Q.PrimVal (Q.X100 _ q) p) t = do
-                                        (X100Res _ res) <- doX100Query c q
-                                        let parted = partByIterX100 res
-                                        [(_, (ListE [n]))] <- constructVector c p parted (ListT t)
-                                        return n
-makeNormX100Prim _ _ _ = error "makeNormX100Prim: Not a primitive value query"
 
 makeNormSQLPrim :: (IConnection conn, Reify a) => conn -> Q.Query Q.SQL -> Type a -> IO (Exp a)
 makeNormSQLPrim c (Q.PrimVal (Q.SQL _ s q) p) t = do
@@ -155,9 +111,6 @@ concatN _                 = error "concatN: Not a list of lists"
 normaliseList :: Type [a] -> Int -> [(Int, [(Int, [SqlValue])])] -> [(Int, Exp [a])]
 normaliseList (ListT t1) c vs = reverse $ foldl' (\tl (i, v) -> (i, ListE (map ((normalise t1 c) . snd) v)):tl) [] vs
 
-normaliseX100List :: Type [a] -> Int -> [(Int, [(Int, [X100Data])])] -> [(Int, Exp [a])]
-normaliseX100List (ListT t1) index vs = reverse $ foldl' (\tl (i, v) -> (i, ListE (map ((normaliseX100 t1) . (!! (index - 1)) . snd) v)):tl) [] vs
-
 normalise :: Type a -> Int -> [SqlValue] -> Exp a
 normalise UnitT _ _ = UnitE
 normalise t i v = convert' (v !! i) t
@@ -189,34 +142,6 @@ convert' (SqlString t) TextT     = TextE (Txt.pack t)
 convert' (SqlByteString s) TextT = TextE (Txt.decodeUtf8 s)
 convert' sql                 _   = error $ "Unsupported SqlValue: "  ++ show sql
 
-normaliseX100 :: Type a -> X100Data -> Exp a
-normaliseX100 UnitT _ = UnitE
-normaliseX100 t v = convert (v, t)
-
-instance Convertible (X100Data, Type a) (Exp a) where
-    safeConvert (Str s, TextT) = Right $ TextE (pack s)
-    safeConvert (Str s, CharT) = Right $ CharE (head s)
-    safeConvert (UChr i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (SChr i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (SInt i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (UInt i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (SSht i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (USht i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (SLng i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (ULng i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (UIDX i, BoolT) = Right $ BoolE (i /= 0)
-    safeConvert (SInt i, IntegerT) = Right $ IntegerE (toInteger i)
-    safeConvert (UInt i, IntegerT) = Right $ IntegerE (toInteger i)
-    safeConvert (SChr i, IntegerT) = Right $ IntegerE (toInteger i)
-    safeConvert (UChr i, IntegerT) = Right $ IntegerE (toInteger i)
-    safeConvert (SSht i, IntegerT) = Right $ IntegerE (toInteger i)
-    safeConvert (USht i, IntegerT) = Right $ IntegerE (toInteger i)
-    safeConvert (SLng i, IntegerT) = Right $ IntegerE i
-    safeConvert (ULng i, IntegerT) = Right $ IntegerE i
-    safeConvert (UIDX i, IntegerT) = Right $ IntegerE i
-    safeConvert (Dbl d, DoubleT) = Right $ DoubleE d
-    safeConvert (d, t) = error $ printf "cannot convert (%s, %s) to Norm" (show d) (show t)
-
 doSQLQuery :: IConnection conn => conn -> String -> IO ([[SqlValue]], [(String, SqlColDesc)])
 doSQLQuery c q = do
                 sth <- prepare c q
@@ -224,9 +149,6 @@ doSQLQuery c q = do
                 res <- dshFetchAllRowsStrict sth
                 resDescr <- describeResult sth
                 return (res, resDescr)
-
-doX100Query :: X100Info -> String -> IO X100Result
-doX100Query c q = executeQuery c q
 
 dshFetchAllRowsStrict :: Statement -> IO [[SqlValue]]
 dshFetchAllRowsStrict stmt = go []
@@ -237,18 +159,6 @@ dshFetchAllRowsStrict stmt = go []
                  Nothing   -> return (reverse acc)
                  Just row  -> do mapM_ evaluate row
                                  go (row : acc)
-
-partByIterX100 :: [X100Column] -> [(Int, [(Int, [X100Data])])]
-partByIterX100 d = pbi d'
-    where
-        d' :: [(Int, Int, [X100Data])]
-        d' = case d of
-                [descr, p] -> zipWith (\x y -> (x, y, [])) (map convert descr) (map convert p)
-                (descr:(p:vs)) -> zip3 (map convert descr) (map convert p) $ transpose vs
-                _          -> $impossible
-        pbi :: [(Int, Int, [X100Data])] -> [(Int, [(Int, [X100Data])])]
-        pbi vs = [ (the i, zip p it) | (i, p, it) <- vs
-                                     , then group by i using groupWith]
 
 partByIter :: Int -> [[SqlValue]] -> [(Int, [(Int, [SqlValue])])]
 partByIter iC vals = pbi (zip [1..] vals)
