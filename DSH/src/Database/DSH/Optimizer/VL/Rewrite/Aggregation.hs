0b95075ecf564ae2e101cbfb2f58d58ab92dd914
Alexander Ulrich <alex@etc-network.de>
2014-01-03 11:21:10 +0100

    Re-implemented rewrites that turn combinations of GroupWith and aggregation operators into a simple GroupAggr operator.

--- DSH/src/Database/DSH/Optimizer/VL/Rewrite/Aggregation.hs
+++ DSH/src/Database/DSH/Optimizer/VL/Rewrite/Aggregation.hs
@@ -17,10 +17,13 @@ import Database.DSH.Optimizer.VL.Properties.Types
 import Database.DSH.Optimizer.VL.Rewrite.Common
 
 aggregationRules :: VLRuleSet ()
-aggregationRules = [ inlineAggrProject ]
+aggregationRules = [ inlineAggrProject
+                   , flatGrouping
+                   ]
 
 aggregationRulesBottomUp :: VLRuleSet BottomUpProps
-aggregationRulesBottomUp = [ pushExprThroughGroupBy ]
+aggregationRulesBottomUp = [ pushExprThroughGroupBy
+                           ]
 
 groupingToAggregation :: VLRewrite Bool
 groupingToAggregation = iteratively $ sequenceRewrites [ applyToAll inferBottomUp aggregationRulesBottomUp
@@ -36,8 +39,6 @@ pushExprThroughGroupBy :: VLRule BottomUpProps
 pushExprThroughGroupBy q = 
   $(pattern 'q "Project projs (qr2=R2 (qg=(qc) GroupBy (qp)))"
     [| do
-        trace ("carnary1 " ++ show q) $ return ()
-  
         [e] <- return $(v "projs")
         case e of
           Column1 _ -> fail "no match"
@@ -77,48 +78,19 @@ inlineAggrProject q =
 
         return $ do
             logRewrite "Aggregation.Normalize.InlineProject" q
-            replaceWithNew q $ BinOp (AggrS afun') $(v "qo") $(v "qi") |])
+            void $ replaceWithNew q $ BinOp (AggrS afun') $(v "qo") $(v "qi") |])
 
           
--- | Turn an aggregate operator into the corrresponding aggregate function for VecAggr
-aggrOpToFun :: DBCol -> VL -> VLMatch () AggrFun
-aggrOpToFun = undefined
-{-
-aggrOpToFun c (UnOp MaxS _)    = return $ AggrMax c
-aggrOpToFun c (UnOp MinS _)    = return $ AggrMin c
-aggrOpToFun c (BinOp AvgS _ _) = return $ AggrAvg c
-aggrOpToFun c (BinOp SumS _ _) = return $ AggrSum c
-aggrOpToFun _ _                = fail "no match"
--}
-        
-{-
-
 -- | Check if we have an operator combination which is eligible for moving to a
--- VecAggr operator.
-matchAggr :: AlgNode -> VLMatch () [(AggrFun, AlgNode)]
+-- GroupAggr operator.
+matchAggr :: AlgNode -> VLMatch () (AggrFun, AlgNode)
 matchAggr q = do
-  op1 <- getOperator q
+  BinOp (AggrS aggrFun) _ _ <- getOperator q
+  return (aggrFun, q)
   
-  -- To change an aggregate operator into an aggregate function, we expect
-  case op1 of
-    -- either a MaxL (ProjectL (R2 GroupBy)) combinaton
-    UnOp (ProjectL [c]) _ -> do
-      ps <- getParents q
-      forM ps $ \p -> do
-        o <- getOperator p
-        f <- aggrOpToFun c o
-        return (f, p)
-
-    -- or LengthSeg (ToDescr (R2 GroupBy))
-    BinOp LengthSeg _ _ -> return [(Count, q)]
-    _                   -> fail "no match"
-  
-
-  
-    
-projectionCol :: PayloadProj -> VLMatch () DBCol
-projectionCol (PLCol c) = return c
-projectionCol _         = fail "no match"
+projectionCol :: Expr1 -> VLMatch () DBCol
+projectionCol (Column1 c) = return c
+projectionCol _           = fail "no match"
 
           
 -- We rewrite a combination of GroupBy and aggregation operators into a single
@@ -127,17 +99,18 @@ projectionCol _         = fail "no match"
 -- 1. The R2 output of GroupBy is only consumed by aggregation operators (MaxL, 
 --    MinL, VecSumL, LengthSeg)
 -- 2. The grouping criteria is a simple column projection from the input vector
-groupingToAggr :: VLRule ()
-groupingToAggr q =
-  $(pattern 'q "R2 (qg=(VLProject ps (_)) GroupBy (q2))"
+flatGrouping :: VLRule ()
+flatGrouping q =
+  $(pattern 'q "R2 (qg=(Project ps (q1)) GroupBy (q2))"
     [| do
-       -- FIXME ensure that both GroupBy inputs have the same origin.
+        -- FIXME this collides with pushProjectThroughGroupBy, as
+        -- there will be a projection underneath the groupby operator
+        -- predicate $ $(v "q1") == $(v "q2")
        
-        groupByParents <- getParents q
-        
         -- We ensure that all parents of the groupBy are operators which we can
         -- turn into aggregate functions
-        funs <- concat <$> mapM matchAggr groupByParents
+        groupByParents <- getParents q
+        funs <- mapM matchAggr groupByParents
         
         -- Check if the grouping criteria are simple columns. Extract the
         -- grouping cols from the left GroupBy input
@@ -154,7 +127,7 @@ groupingToAggr q =
           -- the right input of GroupBy at the same position. In combination
           -- with rewrite pushExprThroughGroupBy, this is true since we only
           -- add columns at the end.
-          aggrNode <- insert $ UnOp (VecAggr groupingCols (map fst funs)) $(v "q2")
+          aggrNode <- insert $ UnOp (GroupAggr groupingCols (map fst funs)) $(v "q2")
 
           -- For every aggregate function, generate a projection which only
           -- leaves the aggregate column. Function receives the node of the
@@ -162,7 +135,7 @@ groupingToAggr q =
           -- aggregation result resides.
           let insertAggrProject :: AlgNode -> DBCol -> VLRewrite ()
               insertAggrProject oldAggrNode aggrCol = 
-                void $ replaceWithNew oldAggrNode $ UnOp (ProjectL [aggrCol]) aggrNode
+                void $ replaceWithNew oldAggrNode $ UnOp (Project [Column1 aggrCol]) aggrNode
 
           zipWithM_ insertAggrProject (map snd funs) [1 .. length funs]
           
@@ -172,8 +145,7 @@ groupingToAggr q =
           r1s <- lookupR1Parents $(v "qg")
           if length r1s > 0
             then do
-              r1ProjectNode <- insert $ UnOp (ProjectL [1 .. length groupingCols]) aggrNode
+              r1ProjectNode <- insert $ UnOp (Project (map Column1 [1 .. length groupingCols])) aggrNode
               mapM_ (\r1 -> replace r1 r1ProjectNode) r1s
             else return () |])
 
--}
