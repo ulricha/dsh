ec2cb69e15b4e592835071de3f1af46fd499a8de
Alexander Ulrich <alex@etc-network.de>
2014-01-05 17:28:55 +0100

    Additional VL rewrite: push and eliminate zip operators through two projections.

--- DSH/src/Database/DSH/Optimizer/VL/Rewrite/Redundant.hs
+++ DSH/src/Database/DSH/Optimizer/VL/Rewrite/Redundant.hs
@@ -24,12 +24,15 @@ cleanup :: VLRewrite Bool
 cleanup = iteratively $ sequenceRewrites [ optExpressions ]
 
 redundantRules :: VLRuleSet ()
-redundantRules = [ introduceSelect, simpleSort ]
+redundantRules = [ introduceSelect
+                 , simpleSort
+                 , zipProject
+                 ]
 
 redundantRulesBottomUp :: VLRuleSet BottomUpProps
 redundantRulesBottomUp = [ distPrimConstant
-                         , pushZipThroughProjectLeft
-                         , pushZipThroughProjectRight
+                         -- , pushZipThroughProjectLeft
+                         -- , pushZipThroughProjectRight
                          , sameInputZip
                          ]
 
@@ -155,8 +158,18 @@ sameInputZip q =
         
         return $ do
           let ps = map Column1 [1 .. w]
+          logRewrite "Redundant.Zip.SameInput" q
           void $ replaceWithNew q $ UnOp (Project (ps ++ ps)) $(v "q1") |])
 
+zipProject :: VLRule ()
+zipProject q =
+  $(pattern 'q "(Project es1 (q1)) Zip (Project es2 (q2))"
+    [| do
+        predicate $ $(v "q1") == $(v "q2")
+
+        return $ do
+          logRewrite "Redundant.Zip.Project" q
+          void $ replaceWithNew q $ UnOp (Project ($(v "es1") ++ $(v "es2"))) $(v "q1") |])
 
 -- | Employ a specialized operator if the sorting criteria are simply
 -- a selection of columns from the input vector.
@@ -167,6 +180,7 @@ simpleSort q =
         predicate $ $(v "q1") == $(v "q2")
 
         return $ do
+          logRewrite "Redundant.Sort.Simple" q
           qs <- insert $ UnOp (SortSimple $(v "ps")) $(v "q1")
           void $ replaceWithNew q $ UnOp R1 qs 
           r2Parents <- lookupR2Parents $(v "qs")
