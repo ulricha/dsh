152f05c3b2e43f52fb52a7e7fb2ff0deac7f7312
Alexander Ulrich <alex@etc-network.de>
2014-03-10 18:04:28 +0100

    More modular structure for test cases.

--- DSH/tests/ComprehensionTests.hs
+++ DSH/tests/ComprehensionTests.hs
@@ -1,81 +1,98 @@
-{-# LANGUAGE RebindableSyntax    #-}
-{-# LANGUAGE ViewPatterns        #-}
-{-# LANGUAGE MonadComprehensions #-}
-    
--- | This module contains testcases for monad comprehensions. We store them in a
--- separate module because they rely on RebindableSyntax and hidden Prelude.
-   
 module ComprehensionTests where
 
-import qualified Prelude as P
-import Database.DSH
-       
----------------------------------------------------------------
--- Comprehensions for quickcheck tests
+import           Common
+import           DSHComprehensions
 
-eqjoin :: Q ([Integer], [Integer]) -> Q [(Integer, Integer)]
-eqjoin (view -> (xs, ys)) = 
-  [ tuple2 x y
-  | x <- xs
-  , y <- ys
-  , x == y
-  ]
+import           Test.QuickCheck
+import           Test.HUnit(Assertion)
+import           Test.Framework (Test, testGroup)
+import           Test.Framework.Providers.QuickCheck2 (testProperty)
+import           Test.Framework.Providers.HUnit
 
-  
-eqjoinproj :: Q ([Integer], [Integer]) -> Q [(Integer, Integer)]
-eqjoinproj (view -> (xs, ys)) = 
-  [ tuple2 x y
-  | x <- xs
-  , y <- ys
-  , (2 * x) == y
-  ]
+tests_comprehensions :: Test
+tests_comprehensions = testGroup "Comprehensions"
+    [ testProperty "eqjoin" prop_eqjoin
+    , testProperty "eqjoinproj" prop_eqjoinproj
+    , testProperty "eqjoinpred" prop_eqjoinpred
+    , testProperty "eqjoin3" prop_eqjoin3
+    , testProperty "eqjoin_nested" prop_eqjoin_nested
+    , testProperty "nestjoin" prop_nestjoin
+    ]
+    
 
-eqjoinpred :: Q (Integer, [Integer], [Integer]) -> Q [(Integer, Integer)]
-eqjoinpred (view -> (x', xs, ys)) = 
-  [ tuple2 x y
-  | x <- xs
-  , y <- ys
-  , x == y
-  , x > x'
-  ]
-
-eqjoin3 :: Q ([Integer], [Integer], [Integer]) -> Q [(Integer, Integer, Integer)]
-eqjoin3 (view -> (xs, ys, zs)) = 
-  [ tuple3 x y z
-  | x <- xs
-  , y <- ys
-  , z <- zs
-  , x == y
-  , y == z
-  ]
-  
-eqjoin_nested :: Q ([(Integer, [Integer])], [Integer]) -> Q [((Integer, [Integer]), Integer)]
-eqjoin_nested args =
-  [ pair x y
-  | x <- fst args
-  , y <- snd args
-  , fst x == y
-  ]
-
-nestjoin :: Q ([Integer], [Integer]) -> Q [(Integer, [Integer])]
-nestjoin (view -> (xs, ys)) =
-  [ tuple2 x [ y | y <- ys, x == y]
-  | x <- xs
-  ]
-  
---------------------------------------------------------------
--- Comprehensions for HUnit tests
-
-eqjoin_nested1 :: Q [((Integer, [Char]), Integer)]
-eqjoin_nested1 =
-    [ pair x y
-    | x <- (toQ ([(10, ['a']), (20, ['b']), (30, ['c', 'd']), (40, [])] :: [(Integer, [Char])]))
-    , y <- (toQ [20, 30, 30, 40, 50])
-    , fst x == y
+tests_join_hunit :: Test
+tests_join_hunit = testGroup "HUnit joins"
+    [ testCase "heqjoin_nested1" heqjoin_nested1
+    , testCase "hsemijoin" hsemijoin
     ]
 
-semijoin :: Q [Integer]
-semijoin = 
-    let xs = (toQ [1, 2, 3, 4, 5, 6, 7] :: Q [Integer])
-        ys = (toQ [2, 4, 6] :: Q [Integer])
-    in [ x | x <- xs , x `elem` ys ]
+---------------------------------------------------------------------------------
+-- QuickCheck properties for comprehensions
+
+prop_eqjoin :: ([Integer], [Integer]) -> Property
+prop_eqjoin = makeProp eqjoin 
+                       (\(xs, ys) -> [ (x, y) | x <- xs , y <- ys , x == y ])
+
+prop_eqjoinproj :: ([Integer], [Integer]) -> Property
+prop_eqjoinproj = 
+  makeProp eqjoinproj (\(xs, ys) -> [ (x, y) | x <- xs , y <- ys , (2 * x) == y ])
+  
+prop_eqjoinpred :: (Integer, [Integer], [Integer]) -> Property
+prop_eqjoinpred =
+  makeProp eqjoinpred
+           (\(x', xs, ys) ->
+             [ (x, y)
+             | x <- xs
+             , y <- ys
+             , x == y
+             , x > x'
+             ])
+
+prop_eqjoin3 :: ([Integer], [Integer], [Integer]) -> Property
+prop_eqjoin3 =
+  makeProp eqjoin3
+           (\(xs, ys, zs) ->
+             [ (x, y, z)
+             | x <- xs
+             , y <- ys
+             , z <- zs
+             , x == y
+             , y == z
+             ])
+             
+prop_eqjoin_nested :: ([(Integer, [Integer])], [Integer]) -> Property
+prop_eqjoin_nested =
+  makeProp eqjoin_nested
+           (\(xs, ys) ->
+             [ (x, y)
+             | x <- xs
+             , y <- ys
+             , fst x == y
+             ])
+
+prop_nestjoin :: ([Integer], [Integer]) -> Property
+prop_nestjoin =
+  makeProp nestjoin
+           (\(xs, ys) ->
+             [ (x, [ y | y <- ys, x == y ])
+             | x <- xs
+             ])
+
+-----------------------------------------------------------------------
+-- HUnit tests for comprehensions
+
+heqjoin_nested1 :: Assertion
+heqjoin_nested1 = makeEqAssertion "heqjoin_nested" eqjoin_nested1 res
+  where
+    res = [ ((20, ['b']), 20)
+          , ((30, ['c', 'd']), 30)
+          , ((30, ['c', 'd']), 30)
+          , ((40, []), 40)
+          ]
+
+hsemijoin :: Assertion
+hsemijoin = makeEqAssertion "hsemijoin" semijoin res
+  where 
+    res = [2, 4, 6]
+    
+  
---
+++ DSH/tests/DSHComprehensions.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE RebindableSyntax    #-}
+{-# LANGUAGE ViewPatterns        #-}
+{-# LANGUAGE MonadComprehensions #-}
+    
+-- | This module contains testcases for monad comprehensions. We store them in a
+-- separate module because they rely on RebindableSyntax and hidden Prelude.
+   
+module DSHComprehensions where
+
+import qualified Prelude as P
+import Database.DSH
+       
+---------------------------------------------------------------
+-- Comprehensions for quickcheck tests
+
+eqjoin :: Q ([Integer], [Integer]) -> Q [(Integer, Integer)]
+eqjoin (view -> (xs, ys)) = 
+  [ tuple2 x y
+  | x <- xs
+  , y <- ys
+  , x == y
+  ]
+
+  
+eqjoinproj :: Q ([Integer], [Integer]) -> Q [(Integer, Integer)]
+eqjoinproj (view -> (xs, ys)) = 
+  [ tuple2 x y
+  | x <- xs
+  , y <- ys
+  , (2 * x) == y
+  ]
+
+eqjoinpred :: Q (Integer, [Integer], [Integer]) -> Q [(Integer, Integer)]
+eqjoinpred (view -> (x', xs, ys)) = 
+  [ tuple2 x y
+  | x <- xs
+  , y <- ys
+  , x == y
+  , x > x'
+  ]
+
+eqjoin3 :: Q ([Integer], [Integer], [Integer]) -> Q [(Integer, Integer, Integer)]
+eqjoin3 (view -> (xs, ys, zs)) = 
+  [ tuple3 x y z
+  | x <- xs
+  , y <- ys
+  , z <- zs
+  , x == y
+  , y == z
+  ]
+  
+eqjoin_nested :: Q ([(Integer, [Integer])], [Integer]) -> Q [((Integer, [Integer]), Integer)]
+eqjoin_nested args =
+  [ pair x y
+  | x <- fst args
+  , y <- snd args
+  , fst x == y
+  ]
+
+nestjoin :: Q ([Integer], [Integer]) -> Q [(Integer, [Integer])]
+nestjoin (view -> (xs, ys)) =
+  [ tuple2 x [ y | y <- ys, x == y]
+  | x <- xs
+  ]
+  
+--------------------------------------------------------------
+-- Comprehensions for HUnit tests
+
+eqjoin_nested1 :: Q [((Integer, [Char]), Integer)]
+eqjoin_nested1 =
+    [ pair x y
+    | x <- (toQ ([(10, ['a']), (20, ['b']), (30, ['c', 'd']), (40, [])] :: [(Integer, [Char])]))
+    , y <- (toQ [20, 30, 30, 40, 50])
+    , fst x == y
+    ]
+
+semijoin :: Q [Integer]
+semijoin = 
+    let xs = (toQ [1, 2, 3, 4, 5, 6, 7] :: Q [Integer])
+        ys = (toQ [2, 4, 6] :: Q [Integer])
+    in [ x | x <- xs , x `elem` ys ]
