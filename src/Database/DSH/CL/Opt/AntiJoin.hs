59f4b08d021943d0fb2ed987d2b59c5ad92505bc
Alexander Ulrich <alex@etc-network.de>
2014-06-20 14:08:34 +0200

    Merged code for universal quantifiers without range predicate into general framework.

--- src/Database/DSH/CL/Opt/AntiJoin.hs
+++ src/Database/DSH/CL/Opt/AntiJoin.hs
@@ -5,72 +5,21 @@ module Database.DSH.CL.Opt.AntiJoin
     ( antijoinR
     ) where
 
-import Debug.Trace
-
 import           Control.Arrow
 import           Control.Applicative
 import           Data.List.NonEmpty         (NonEmpty ((:|)))
 import           Data.Semigroup
 import qualified Data.Traversable as T
 import Data.List
-import qualified Data.List.NonEmpty as NL
 
 import           Database.DSH.CL.Kure
 import           Database.DSH.CL.Lang
 import           Database.DSH.CL.Opt.Aux
 import qualified Database.DSH.CL.Primitives as P
 import           Database.DSH.Common.Lang
-import           Database.DSH.Impossible
 
 --------------------------------------------------------------------------------
--- Introduce anti joins (universal quantification)
-
---------------------------------------------------------------------------------
--- Basic antijoin pattern
-
--- | Construct an antijoin qualifier given a predicate and two generators. Note
--- that the splitJoinPred call implicitly checks that only x and y occur free in
--- the predicate and no further correlation takes place.
-mkantijoinT :: Expr -> Ident -> Ident -> Expr -> Expr -> TransformC (NL Qual) Qual
-mkantijoinT joinPred x y xs ys = do
-    joinConjunct <- constT (return joinPred) >>> splitJoinPredT x y
-
-    let xst = typeOf xs
-        yst = typeOf ys
-        jt  = xst .-> yst .-> xst
-
-    -- => [ ... | ..., x <- xs antijoin(p1, p2) ys, ... ]
-    return $ BindQ x (AppE2 xst (Prim2 (AntiJoin $ singlePred joinConjunct) jt) xs ys)
-
--- | Match the basic antijoin pattern in the middle of a qualifier list. This is
--- essentially the operator definition, generalized to multiple qualifiers and
--- an arbitrary comprehension head:
--- [ f x | qs, x <- xs, and [ not (q y) | y <- ys ], qs' ]
--- => [ f x | qs, x <- xs antijoin(q) ys, qs' ]
-basicAntiJoinR :: RewriteC (NL Qual)
-basicAntiJoinR = do
-    -- [ ... | ..., x <- xs, and [ not p | y <- ys ], ... ]
-    BindQ x xs :* GuardQ (AppE1 _ (Prim1 And _)
-                                  (Comp _ (UnOp _ (SUBoolOp Not) p)
-                                          (S (BindQ y ys))))  :* qs <- idR
-    q' <- mkantijoinT p x y xs ys
-    return $ q' :* qs
-
--- | Match a NOT IN antijoin pattern at the end of a list
-basicAntiJoinEndR :: RewriteC (NL Qual)
-basicAntiJoinEndR = do
-    -- [ ... | ..., x <- xs, and [ True | y <- ys, not p ] ]
-    BindQ x xs :* S (GuardQ (AppE1 _ (Prim1 And _)
-                                     (Comp _ (UnOp _ (SUBoolOp Not) p)
-                                             (S (BindQ y ys))))) <- idR
-    q' <- mkantijoinT p x y xs ys
-    return (S q')
-
---------------------------------------------------------------------------------
--- Doubly Negated existential quantifier (NOT EXISTS)
-
---------------------------------------------------------------------------------
--- Universal quantification with range predicates
+-- Universal quantification with and without range predicates
 
 -- | Turn universal quantification with range and quantifier predicates into an
 -- antijoin. We use the classification of queries in Claussen et al.: Optimizing
@@ -105,7 +54,9 @@ negateRelOp op = case op of
 
 -- | Quantifier predicates that reference inner and outer relation
 -- appear negated on the antijoin.
-quantifierPredicateT :: Ident -> Ident -> TransformC CL (NonEmpty (JoinConjunct JoinExpr))
+quantifierPredicateT :: Ident 
+                     -> Ident 
+                     -> TransformC CL (NonEmpty (JoinConjunct JoinExpr))
 quantifierPredicateT x y = readerT $ \q -> case q of
     -- If the quantifier predicate is already negated, take its
     -- non-negated form.
@@ -125,23 +76,35 @@ quantifierPredicateT x y = readerT $ \q -> case q of
 
 universalQualR :: RewriteC (NL Qual)
 universalQualR = readerT $ \qs -> case qs of
+    -- Special case: no range predicate
+    -- [ ... | ..., x <- xs, and [ q | y <- ys ]]
+    BindQ x xs :* (S (GuardQ (PAnd (Comp _ q (S (BindQ y ys)))))) -> do
+        qPred <- constT (return q) >>> injectT >>> quantifierPredicateT x y
+        return $ S $ BindQ x (P.antijoin xs ys $ JoinPred $ qPred)
+
+    -- Special case: no range predicate
+    -- [ ... | ..., x <- xs, and [ q | y <- ys ], ... ]
+    BindQ x xs :* (GuardQ (PAnd (Comp _ q (S (BindQ y ys))))) :* qs -> do
+        qPred <- constT (return q) >>> injectT >>> quantifierPredicateT x y
+        return $ (BindQ x (P.antijoin xs ys $ JoinPred $ qPred)) :* qs
+
     -- [ ... | ..., x <- xs, and [ q | y <- ys, ps ], ... ]
     BindQ x xs :* GuardQ (PAnd (Comp _ q (BindQ y ys :* ps))) :* qs -> do
-        antijoinGen <- mkUniversalAntiJoinT (x, xs) (y, ys) ps q
+        antijoinGen <- mkUniversalRangeAntiJoinT (x, xs) (y, ys) ps q
         return $ antijoinGen :* qs
 
     -- [ ... | ..., x <- xs, and [ q | y <- ys, ps ]]
     BindQ x xs :* (S (GuardQ (PAnd (Comp _ q (BindQ y ys :* ps))))) -> do
-        antijoinGen <- mkUniversalAntiJoinT (x, xs) (y, ys) ps q
+        antijoinGen <- mkUniversalRangeAntiJoinT (x, xs) (y, ys) ps q
         return $ S $ antijoinGen
     _ -> fail "no and pattern"
 
-mkUniversalAntiJoinT :: (Ident, Expr) 
+mkUniversalRangeAntiJoinT :: (Ident, Expr) 
                      -> (Ident, Expr)
                      -> NL Qual
                      -> Expr
                      -> TransformC (NL Qual) Qual
-mkUniversalAntiJoinT (x, xs) (y, ys) ps q = do
+mkUniversalRangeAntiJoinT (x, xs) (y, ys) ps q = do
     psExprs <- constT $ T.mapM fromGuard ps
     let psFVs = sort $ nub $ concatMap freeVars $ toList psExprs
         qFVs  = sort $ nub $ freeVars q
