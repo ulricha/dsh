2138aaf7dd3a3e64e956865b2d735c051b32ba6a
Alexander Ulrich <alex@etc-network.de>
2014-07-17 09:35:04 +0200

    Loop-invariant expressions in guards don't have to be closed to be extracted. It is sufficient if none of the variables bound in the current comprehension occur free.

--- src/Database/DSH/CL/Opt/LoopInvariant.hs
+++ src/Database/DSH/CL/Opt/LoopInvariant.hs
@@ -11,6 +11,8 @@ module Database.DSH.CL.Opt.LoopInvariant
   
 import           Control.Applicative
 import           Control.Arrow
+import           Data.Maybe
+import           Data.List
 
 import           Database.DSH.Common.Lang
 import           Database.DSH.CL.Lang
@@ -19,8 +21,8 @@ import qualified Database.DSH.CL.Primitives as P
 import           Database.DSH.CL.Opt.Aux
 
 -- | Collect a path to a complex expression
-complexPathT :: TransformC CL [(Expr, PathC)]
-complexPathT = do
+complexPathT :: [Ident] -> TransformC CL [(Expr, PathC)]
+complexPathT localVars = do
     ExprCL e <- idR
     -- debugPretty "complexPathT" e
     path <- snocPathToPath <$> absPathT
@@ -29,17 +31,12 @@ complexPathT = do
     -- We are only interested in constant expressions that do not
     -- depend on variables bound by generators in the enclosing
     -- comprehension.
-    -- 
-    -- FIXME it would not hurt to allow variables that are bound
-    -- outside of the current comprehension. It's only relevant that
-    -- the expression is constant "relative to" the current
-    -- comprehension.
-    --
+    guardM $ null $ freeVars e `intersect` localVars
+
+    let ret = return [(e, path)]
     -- FIXME more precise heuristics could be employed: A
     -- comprehension is only "complex" if it has more than one
     -- generator OR a filter OR something complex in the head.
-    guardM $ null $ freeVars e
-    let ret = return [(e, path)]
     case e of
         Comp _ _ _                                 -> ret
         If _ _ _ _                                 -> ret
@@ -49,10 +46,10 @@ complexPathT = do
 
 -- | In a given guard expression, search for a complex loop-invariant
 -- sub-expression and move it to a generator.
-invariantExprT :: TransformC CL (Ident, Expr, Expr)
-invariantExprT = do
+invariantExprT :: [Ident] -> TransformC CL (Ident, Expr, Expr)
+invariantExprT localVars = do
     -- Collect largest complex expressions in all childs
-    candidateExprs <- oneT $ onetdT complexPathT
+    candidateExprs <- oneT $ onetdT (complexPathT localVars)
 
     -- choose the first candidate
     (complexExpr, complexPath) : _ <- return candidateExprs
@@ -74,19 +71,23 @@ invariantExprT = do
 
     return (x, singletonExpr, simplifiedPred)
 
-invariantQualR :: RewriteC (NL Qual)
-invariantQualR = do
+invariantQualR :: [Ident] -> RewriteC (NL Qual)
+invariantQualR localVars = do
     readerT $ \case
         GuardQ p :* qs -> do
-            (x, xs, p') <- constT (return $ inject $ p) >>> invariantExprT
+            (x, xs, p') <- constT (return $ inject $ p) >>> (invariantExprT localVars)
             return $ BindQ x xs :* GuardQ p' :* qs
         S (GuardQ p) -> do
-            (x, xs, p') <- constT (return $ inject $ p) >>> invariantExprT
+            (x, xs, p') <- constT (return $ inject $ p) >>> (invariantExprT localVars)
             return $ BindQ x xs :* (S $ GuardQ p')
         _ -> fail "no match"
 
 loopInvariantGuardR :: RewriteC CL
 loopInvariantGuardR = do
     Comp t h qs <- promoteT idR
-    qs' <- constT (return qs) >>> onetdR invariantQualR
+    -- FIXME passing *all* generator variables in the current
+    -- comprehension is too conservative. It would be sufficient to
+    -- consider those preceding the guard that is under investigation.
+    let genVars = fmap fst $ catMaybes $ fmap fromGen $ toList qs
+    qs' <- constT (return qs) >>> onetdR (invariantQualR genVars)
     return $ inject $ Comp t h qs'
