7f6148dbbe2a895346910d1a4478b5f3efc9342f
Alexander Ulrich <alex@etc-network.de>
2014-08-01 16:57:04 +0200

    Better method of serializing NonEmpty to JSON.

--- src/Database/DSH/VL/Render/JSON.hs
+++ src/Database/DSH/VL/Render/JSON.hs
@@ -3,19 +3,20 @@
 
 module Database.DSH.VL.Render.JSON(serializePlan, deserializePlan, planToFile, planFromFile) where
 
-import           GHC.Generics(Generic)
-import qualified Data.IntMap as M
-import qualified Data.List.NonEmpty as N
 import           Control.Monad
 import           Data.Functor
+import qualified Data.IntMap                 as M
+import           Data.List.NonEmpty          (NonEmpty (..))
+import           GHC.Generics                (Generic)
 
-import qualified Data.ByteString.Lazy.Char8 as BL
-import           Data.Aeson (ToJSON, FromJSON, toJSON, parseJSON, encode, decode)
+import           Data.Aeson                  (FromJSON, ToJSON, decode, encode,
+                                              parseJSON, toJSON)
+import qualified Data.ByteString.Lazy.Char8  as BL
 
 import           Database.Algebra.Dag.Common
 
+import qualified Database.DSH.Common.Lang    as L
 import           Database.DSH.Impossible
-import qualified Database.DSH.Common.Lang as L
 import           Database.DSH.VL.Lang
 
 instance ToJSON TerOp where
@@ -42,9 +43,9 @@ instance ToJSON L.ColName where
 instance ToJSON L.TableHints where
 instance ToJSON e => ToJSON (L.JoinConjunct e)
 instance ToJSON e => ToJSON (L.JoinPredicate e)
-instance ToJSON a => ToJSON (N.NonEmpty a) where
-    toJSON nl = toJSON $ N.toList nl
-  
+instance ToJSON a => ToJSON (NonEmpty a) where
+    toJSON (n :| nl) = toJSON (n, nl)
+
 
 instance FromJSON TerOp where
 instance FromJSON BinOp where
@@ -70,20 +71,15 @@ instance FromJSON L.Key where
 instance FromJSON L.TableHints
 instance FromJSON e => FromJSON (L.JoinConjunct e)
 instance FromJSON e => FromJSON (L.JoinPredicate e)
-instance FromJSON a => FromJSON (N.NonEmpty a) where
-    parseJSON doc = fromList <$> parseJSON doc
-
-fromList :: [a] -> N.NonEmpty a
-fromList [] = $impossible
-fromList (x : xs) = x N.:| xs
-            
+instance FromJSON a => FromJSON (NonEmpty a) where
+    parseJSON doc = parseJSON doc >>= \(n, nl) -> return $ n :| nl
 
 instance ToJSON Plan where
 instance FromJSON Plan where
-   
+
 data Plan = Plan {tags :: [(AlgNode, [Tag])], roots :: [AlgNode], graph :: [(AlgNode, VL)]}
     deriving Generic
-             
+
 serializePlan :: (NodeMap [Tag], [AlgNode], NodeMap VL) -> BL.ByteString
 serializePlan (ts, rs, g) = let tags' = M.toList ts
                                 graph' = M.toList g
@@ -92,7 +88,7 @@ serializePlan (ts, rs, g) = let tags' = M.toList ts
 deserializePlan :: BL.ByteString -> (NodeMap [Tag], [AlgNode], NodeMap VL)
 deserializePlan s = let Just (Plan ts rs g) = decode s
                     in (M.fromList ts, rs, M.fromList g)
-    
+
 planToFile :: FilePath -> (NodeMap [Tag], [AlgNode], NodeMap VL) -> IO ()
 planToFile f t = BL.writeFile f $ serializePlan t
 
