ae0a66e0a2bef97ce3d20105cea916ffc31846c8
Alexander Ulrich <alex@etc-network.de>
2014-12-13 17:02:41 +0100

    TPC-H Examples.

--- examples/Example03.hs
+++ examples/Example03.hs
@@ -9,6 +9,13 @@
 {-# LANGUAGE UndecidableInstances  #-}
 {-# LANGUAGE ViewPatterns          #-}
 
+-- | A number of more complex DSH examples based on the TPC-H
+-- benchmark schema. A data generator is available at
+-- <http://www.tpc.org/tpch/>. Note that DSH currently does not
+-- support temporal and decimal types. Those have to be mapped to
+-- epoch integers and doubles, respectively. The script
+-- 'examples/dshify-tpch.sql' modifies a PostgreSQL TPCH database
+-- accordingly.
 module Main where
 
 import qualified Prelude as P
@@ -17,85 +24,132 @@ import Database.DSH.Compiler
 
 import Database.HDBC.PostgreSQL
 
-data Employee = Prof  { name            :: Text
-                      , chair           :: Text
-                      , advisedStudents :: [Text]
-                      }
-              | Stud  { name    :: Text
-                      , topic   :: Text
-                      , advisor :: Text
-                      }
-              deriving (Eq,Ord,Show)
+-- | We declare a flat record type that maps to the relational
+-- schema. Names of record selectors should match attribute names in
+-- the schema. Note that record selectors should be listed in
+-- alphabetical order.
+data LineItem = LineItem
+    { l_comment       :: Text
+    , l_commitdate    :: Integer
+    , l_discount      :: Double
+    , l_extendedprice :: Double
+    , l_linenumber    :: Integer
+    , l_linestatus    :: Text
+    , l_orderkey      :: Integer
+    , l_partkey       :: Integer
+    , l_quantity      :: Double
+    , l_receiptdate   :: Integer
+    , l_returnflag    :: Text
+    , l_shipdate      :: Integer
+    , l_shipinstruct  :: Text
+    , l_shipmode      :: Text
+    , l_suppkey       :: Integer
+    , l_tax           :: Double
+    }
+    deriving (Show)
 
-deriveDSH ''Employee
+-- A bit of Template Haskell code automatically derives instances and
+-- infrastructure to use the record type in DSH queries. For each
+-- record selector, we derive a variant that can be used in
+-- queries. For example, we get a lifted record selector 
+-- 'l_commentQ :: Q LineItem -> Q Text'.
+deriveDSH ''LineItem
+deriveTA ''LineItem
+generateTableSelectors ''LineItem
 
-students :: Q [(Integer,Text,Text,Text)]
-students = toQ  [ (1,"J","P","T")
-                , (2,"A","Q","T")
-                ]
+-- | The 'table' combinator implements a database table scan.
+lineitems :: Q [LineItem]
+lineitems = table "lineitem" $ TableHints [Key ["l_orderkey", "l_linenumber"]] NonEmpty
 
-professors :: Q [(Integer,Text,Text)]
-professors = toQ  [ (0,"T","DB")]
+data Order = Order
+    { o_clerk         :: Text
+    , o_comment       :: Text
+    , o_custkey       :: Integer
+    , o_orderdate     :: Integer
+    , o_orderkey      :: Integer
+    , o_orderpriority :: Text
+    , o_orderstatus   :: Text
+    , o_shippriority  :: Integer
+    , o_totalprice    :: Double
+    }
+    deriving (Show)
 
-employment :: Q [(Integer,Text,Text,Integer)]
-employment = toQ  [ (0,"DB","professor",2008)
-                  , (1,"DB","student",2010)
-                  , (2,"DB","student",2011)
-                  , (3,"PL","student",2012)
-                  ]
+deriveDSH ''Order
+deriveTA ''Order
+generateTableSelectors ''Order
 
-salaries :: Q [(Integer,Integer)]
-salaries = toQ  [ (0,4096)
-                , (1,2048)
-                , (2,2048)
-                ]
+orders :: Q [Order]
+orders = table "orders" $ TableHints [Key ["o_orderkey"]] NonEmpty
 
-employeesBySeniority :: Q [Employee]
-employeesBySeniority = concat
-  [ if eStatus == "student"
-       then  [ stud sName sTopic sAdvisor
-             | (view -> (sID, sName, sTopic, sAdvisor)) <- students
-             , eID == sID
-             ]
-       else  [  prof pName pChair sAdvised
-             |  (view -> (pID, pName, pChair)) <- professors
-             ,  eID == pID
-             ,  let sAdvised = [ sName
-                               | (view -> (_, sName, _, sAdvisor)) <- students
-                               , sAdvisor == pName
-                               ]
-             ]
-  | (view -> (eID, _, eStatus, _)) <- sortWith (\(view -> (_, _, _, d)) -> d) employment
-  ]
+--------------------------------------------------------------------------------
 
-safeMinimum :: (Ord a, QA a) => Q [a] -> Q (Maybe a)
-safeMinimum as = if null as then nothing else just (minimum as) 
+-- Select all lineitems that were shipped before a given date
+ordersBefore :: Q Integer -> Q [LineItem]
+ordersBefore date = [ li | li <- lineitems , l_shipdateQ li <= date ]
 
-salPerDept :: Q [(Text, [Integer])]
-salPerDept =
-  [ pair dept [ salary
-              | (view -> (sID,salary)) <- salaries
-              , (view -> (dID,_,_,_)) <- deptMembers
-              , sID == dID
-              ]
-  | (view -> (dept, deptMembers)) <- groupWithKey (\(view -> (_,d,_,_)) -> d) employment
-  ]
+fst9 :: (QA a, QA b, QA c, QA d, QA e, QA f, QA g, QA h, QA i) => Q (a, b, c, d, e, f, g, h, i) -> Q a
+fst9 (view -> (a, _, _, _, _, _, _, _, _)) = a
 
-minSalPerDept :: Q [(Text, Integer)]
-minSalPerDept =  [ pair dept (elim (safeMinimum sals)
-                                   0
-                                   (\minSal -> minSal))
-                 | (view -> (dept, sals)) <- salPerDept
-                 ]
+-- Compute the revenue of a single lineitem
+revenue :: Q LineItem -> Q Double
+revenue li = l_extendedpriceQ li * (1 - l_discountQ li)
+
+-- TPC-H benchmark query Q1
+q1 :: Q Integer 
+   -> Q [((Text, Text), Double, Double, Double, Double, Double, Double, Double, Integer)]
+q1 maxDate = sortWith fst9 $ 
+     [ tup9
+	  k
+	  (sum $ map l_quantityQ lis)
+	  (sum $ map l_extendedpriceQ lis)
+	  (sum $ map revenue lis)
+	  (sum $ map (\li -> revenue li * (1 + l_taxQ li)) lis)
+	  (avg $ map l_quantityQ lis)
+	  (avg $ map l_extendedpriceQ lis)
+	  (avg $ map l_discountQ lis)
+	  (length lis)
+      | (view -> (k, lis)) <- groupWithKey (\li -> pair (l_returnflagQ li) (l_linestatusQ li)) 
+                              $ ordersBefore maxDate
+      ]
+
+--------------------------------------------------------------------------------
+
+data Range = Range { start :: Integer, end :: Integer }
+
+inside :: Q Integer -> Range -> Q Bool
+inside d range = d >= toQ (start range) && d < toQ (end range)
+
+lineItemsOf :: Q Order -> Q [LineItem]
+lineItemsOf o = [ l | l <- lineitems,
+                      l_orderkeyQ l == o_orderkeyQ o ]
+
+-- Has at least one of the orders' items been delivered late?
+hasLateItem :: Q Order -> Q Bool
+hasLateItem o =
+  or [ l_commitdateQ l < l_receiptdateQ l | l <- lineItemsOf o ]
+
+-- Compute the number of delayed orders per priority level in the
+-- given quarter (TPC-H benchmark query Q4).
+q4 :: Range -> Q [(Text, Integer)]
+q4 quarter = sortWith fst
+  [ pair priority (length delays)
+  | (view -> (priority, delays)) <- groupWithKey o_orderpriorityQ delayedOrders ]
+  where
+    delayedOrders = [ o | o <- orders
+                    , o_orderdateQ o `inside` quarter
+                    , hasLateItem o
+                    ]
+
+--------------------------------------------------------------------------------
 
 getConn :: IO Connection
-getConn = connectPostgreSQL "user = 'giorgidz' password = '' host = 'localhost' dbname = 'giorgidz'"
+getConn = connectPostgreSQL "user = 'au' password = 'foobar' host = 'localhost' dbname = 'tpchsmall'"
 
-runQ :: (Show a,QA a) => Q a -> IO ()
-runQ q = getConn P.>>= \conn -> (fromQ conn q P.>>= P.print) P.>> disconnect conn
+execQ :: (Show a,QA a) => Q a -> IO ()
+execQ q = getConn P.>>= \conn -> (runQ conn q P.>>= P.print) P.>> disconnect conn
 
 main :: IO ()
-main = sequence_  [ runQ employeesBySeniority
-                  , runQ salPerDept
-                  , runQ minSalPerDept
+main = sequence_  [ execQ $ q1 904663977
+                  -- Compute Q4 for a three-month interval
+                  , execQ (q4 $ Range 741540777 749489577)
                   ]
