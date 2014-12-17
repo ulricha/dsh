74717b6c81a0ec0578ec63d2be14eb14f9fe2867
Alexander Ulrich <alex@etc-network.de>
2014-12-17 21:47:23 +0100

    sandbox.

--- mksandbox.sh
+++ mksandbox.sh
@@ -5,8 +5,8 @@ cabal sandbox init
 
 cabal sandbox add-source $HOME/work/dev/algebra-dag
 cabal sandbox add-source $HOME/work/dev/algebra-sql
-#cabal sandbox add-source $HOME/work/dev/algebra-x100
-#cabal sandbox add-source $HOME/work/dev/x100client
-cabal install --dependencies-only --disable-library-profiling --disable-executable-profiling # --extra-lib-dirs $HOME/software/x100/lib
+cabal sandbox add-source $HOME/work/dev/algebra-x100
+cabal sandbox add-source $HOME/work/dev/x100client
+cabal install --dependencies-only --disable-library-profiling --disable-executable-profiling --extra-lib-dirs $HOME/software/x100/lib
 cabal configure --disable-library-profiling --disable-executable-profiling 
 
