a0f0a569c13454239c584052ca9bcf7256ab7adf
Alexander Ulrich <alex@etc-network.de>
2015-01-05 12:22:32 +0100

    --disable-*-profiling seems not to be supported in cabal 1.22

--- mksandbox.sh
+++ mksandbox.sh
@@ -7,6 +7,6 @@ cabal sandbox add-source $HOME/work/dev/algebra-dag
 cabal sandbox add-source $HOME/work/dev/algebra-sql
 cabal sandbox add-source $HOME/work/dev/algebra-x100
 cabal sandbox add-source $HOME/work/dev/x100client
-cabal install --dependencies-only --disable-library-profiling --disable-executable-profiling --extra-lib-dirs $HOME/software/x100/lib
-cabal configure --disable-library-profiling --disable-executable-profiling 
+cabal install --dependencies-only --extra-lib-dirs $HOME/software/x100/lib
+cabal configure
 
