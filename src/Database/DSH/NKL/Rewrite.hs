f2683c98588bf22e6c69c7da90b3874287216c77
Alexander Ulrich <alex@etc-network.de>
2015-02-18 09:57:16 +0100

    Simple rewrites on desugared NKL terms.
    1. Merge enclosing iterators into sorting (pull up sort operators)
    2. Merge adjacent restrict operators into conjunctive predicates.

--- src/Database/DSH/NKL/Rewrite.hs
+++ src/Database/DSH/NKL/Rewrite.hs
@@ -9,17 +9,18 @@ module Database.DSH.NKL.Rewrite
     , optimizeNKL
     ) where
 
-import Control.Arrow
-import Data.List
-import Data.Monoid
+import           Control.Arrow
+import           Data.List
+import           Data.Monoid
 
-import Database.DSH.Common.Impossible
-import Database.DSH.Common.Type
-import Database.DSH.Common.Lang
-import Database.DSH.Common.Kure
-import Database.DSH.Common.RewriteM
-import Database.DSH.NKL.Kure
-import Database.DSH.NKL.Lang
+import           Database.DSH.Common.Impossible
+import           Database.DSH.Common.Kure
+import           Database.DSH.Common.Lang
+import           Database.DSH.Common.RewriteM
+import           Database.DSH.Common.Type
+import           Database.DSH.NKL.Kure
+import           Database.DSH.NKL.Lang
+import qualified Database.DSH.NKL.Primitives    as P
 
 -- | Run a translate on an expression without context
 applyExpr :: [Ident] -> TransformN Expr b -> Expr -> Either String b
@@ -56,12 +57,12 @@ subst :: [Ident] -> Ident -> Expr -> Expr -> Expr
 subst nameCtx x s e = either (const e) id $ applyExpr nameCtx (substR x s) e
 
 alphaCompR :: [Ident] -> RewriteN Expr
-alphaCompR avoidNames = do 
+alphaCompR avoidNames = do
     Iterator compTy h x _  <- idR
     x'                     <- freshNameT (x : freeVars h ++ avoidNames)
     let varTy = elemT compTy
-    iteratorT (tryR $ substR x (Var varTy x')) 
-              idR 
+    iteratorT (tryR $ substR x (Var varTy x'))
+              idR
               (\_ h' _ xs' -> Iterator compTy h' x' xs')
 
 alphaLetR :: [Ident] -> RewriteN Expr
@@ -129,8 +130,9 @@ inlineBindingR v s = readerT $ \expr -> case expr of
     _                         -> anyR $ inlineBindingR v s
 
 pattern ConcatP t xs <- AppE1 t Concat xs
-pattern SingletonP e <- AppE1 _ Singleton e 
-       
+pattern SingletonP e <- AppE1 _ Singleton e
+pattern RestrictP e  <- AppE1 _ Restrict e
+
 -- concatMap (\x -> [e x]) xs
 -- concat [ [ e x ] | x <- xs ]
 -- =>
@@ -147,7 +149,7 @@ countVarRefT v = readerT $ \expr -> case expr of
     Var _ n | n == v         -> return 1
     Var _ _ | otherwise      -> return 0
 
-    Let _ n _ _ | n == v     -> letT (constT $ return 0) 
+    Let _ n _ _ | n == v     -> letT (constT $ return 0)
                                      (countVarRefT v)
                                      (\_ _ c1 c2 -> c1 + c2)
     Let _ _ _ _ | otherwise  -> letT (countVarRefT v)
@@ -198,11 +200,48 @@ simpleBindingR = do
     guardM $ simpleExpr e1
     childT LetBody $ substR x e1
 
+-- | Eliminate an iterator that does not perform any work.
 identityIteratorR :: RewriteN Expr
 identityIteratorR = do
     Iterator _ (Var _ x) x' xs <- idR
     guardM $ x == x'
     return xs
+
+-- | Push an iterator expression into a Sort operator to get a more
+-- compact NKL expression. Note: Effectively, this rewrite pulls up
+-- sorting in the plan. For sorting, this is propably OK. For
+-- Restrict, though, that would not be a good idea.
+--
+-- [ f x | x <- sort [ (g y, h y) | y <- ys ] ]
+-- =>
+-- sort [ (f [g y/x], h y) | y <- ys ]
+mergeSortIteratorR :: RewriteN Expr
+mergeSortIteratorR = do
+    Iterator _ f x (AppE1 _ Sort (Iterator _ (MkTuple _ [g, h]) y ys)) <- idR
+    g' <- constT (return f) >>> substR x g
+    let ft = typeOf f
+        pt = TupleT [ft, BoolT]
+    return $ AppE1 (ListT ft) Sort (Iterator (ListT pt) (MkTuple pt [g', h]) y ys)
+
+-- | Merge two adjacent restricts into one.
+--
+-- restrict [ (x, p1 x) | x <- restrict [ (y, p2 y) | y <- ys ] ]
+-- =>
+-- restrict [ (y, p1[y/x] y && p2 y) | y <- ys ]
+mergeRestrictR :: RewriteN Expr
+mergeRestrictR = do
+    RestrictP (Iterator _ (MkTuple _ [Var _ x', p1])
+                          x
+                          (RestrictP (Iterator _ (MkTuple _ [Var _ y', p2])
+                                     y
+                                     ys)))                                  <- idR
+    guardM $ x == x'
+    guardM $ y == y'
+    let yt  = elemT $ typeOf ys
+        yst = ListT yt
+    p1' <- constT (return p1) >>> substR x (Var yt y)
+    let p = BinOp BoolT (SBBoolOp Conj) p1' p2
+    return $ P.restrict (Iterator yst (P.tuple [Var yt y, p]) y ys)
 
 nklOptimizations :: RewriteN Expr
 nklOptimizations = anybuR $ singletonHeadR
@@ -210,9 +249,11 @@ nklOptimizations = anybuR $ singletonHeadR
                             <+ referencedOnceR
                             <+ simpleBindingR
                             <+ identityIteratorR
+                            <+ mergeSortIteratorR
+                            <+ mergeRestrictR
 
 optimizeNKL :: Expr -> Expr
 optimizeNKL expr = debugOpt "NKL" expr optimizedExpr
   where
     optimizedExpr = applyExpr [] nklOptimizations expr
-        
+
