53f9ba15912bf1c41da30ff5be7a6e643ab903ca
Alexander Ulrich <alex@etc-network.de>
2015-04-21 16:23:38 +0200

    Disable empty rewrites (not correct for natural keys).
    Pruning empty data vectors alone is not correct in the presence of
    natural keys: Re-keying changes inner vector references.

--- src/Database/DSH/VL/Opt/Rewrite/PruneEmpty.hs
+++ src/Database/DSH/VL/Opt/Rewrite/PruneEmpty.hs
@@ -9,19 +9,24 @@ import           Database.DSH.VL.Opt.Properties.Types
 import           Database.DSH.VL.Opt.Rewrite.Common
 
 import           Database.Algebra.Dag.Common
-import           Database.DSH.VL.Lang
+-- import           Database.DSH.VL.Lang
 
 pruneEmpty :: VLRewrite Bool
 pruneEmpty = applyToAll inferBottomUp emptyRules
 
 emptyRules :: VLRuleSet BottomUpProps
-emptyRules = [ emptyAppendLeftR1
+emptyRules = [ -- emptyAppendLeftR1
              -- , emptyAppendLeftR2
              -- , emptyAppendLeftR3
-             , emptyAppendRightR1
+             -- , emptyAppendRightR1
              -- , emptyAppendRightR2
              -- , emptyAppendRightR3
              ]
+
+-- FIXME pruning data vectors (R1) alone is not sufficient when
+-- dealing with natural keys. We need to treat R2 and R3 outputs as
+-- well, because otherwise inner vectors will be re-keyed and no
+-- longer be aligned with the outer vector.
 
 isEmpty :: AlgNode -> VLMatch BottomUpProps Bool
 isEmpty q = do
@@ -30,17 +35,17 @@ isEmpty q = do
     VProp b -> return b
     x       -> error $ "PruneEmpty.isEmpty: non-vector input " ++ show x
 
-{- If the left input is empty and the other is not, the resulting value vector
-is simply the right input. -}
-emptyAppendLeftR1 :: VLRule BottomUpProps
-emptyAppendLeftR1 q =
-  $(dagPatMatch 'q "R1 ((q1) [Append | AppendS] (q2))"
-    [| do
-        predicate =<< ((&&) <$> (isEmpty $(v "q1")) <*> (not <$> isEmpty $(v "q2")))
+-- {- If the left input is empty and the other is not, the resulting value vector
+-- is simply the right input. -}
+-- emptyAppendLeftR1 :: VLRule BottomUpProps
+-- emptyAppendLeftR1 q =
+--   $(dagPatMatch 'q "R1 ((q1) [Append | AppendS] (q2))"
+--     [| do
+--         predicate =<< ((&&) <$> (isEmpty $(v "q1")) <*> (not <$> isEmpty $(v "q2")))
 
-        return $ do
-          logRewrite "Empty.Append.Left.R1" q
-          replace q $(v "q2") |])
+--         return $ do
+--           logRewrite "Empty.Append.Left.R1" q
+--           replace q $(v "q2") |])
 
 -- FIXME re-add rules when
 {-
@@ -71,14 +76,14 @@ emptyAppendLeftR3 q =
           replace q $(v "qv") |])
 -}
 
-emptyAppendRightR1 :: VLRule BottomUpProps
-emptyAppendRightR1 q =
-  $(dagPatMatch 'q "R1 ((q1) [Append | AppendS] (q2))"
-    [| do
-        predicate =<< ((&&) <$> (isEmpty $(v "q2")) <*> (not <$> isEmpty $(v "q1")))
-        return $ do
-          logRewrite "Empty.Append.Right.R1" q
-          replace q $(v "q1") |])
+-- emptyAppendRightR1 :: VLRule BottomUpProps
+-- emptyAppendRightR1 q =
+--   $(dagPatMatch 'q "R1 ((q1) [Append | AppendS] (q2))"
+--     [| do
+--         predicate =<< ((&&) <$> (isEmpty $(v "q2")) <*> (not <$> isEmpty $(v "q1")))
+--         return $ do
+--           logRewrite "Empty.Append.Right.R1" q
+--           replace q $(v "q1") |])
 
 {-
 -- If the right input is empty, renaming will make the inner vector
