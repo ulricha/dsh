9ebc326477ba1925696d7b8d96fd2f6ed8d1451d
Alexander Ulrich <alex@etc-network.de>
2015-05-26 22:13:13 +0200

    don't need local version of ansi-wl-pprint anymore

--- utils/mksandbox.sh
+++ utils/mksandbox.sh
@@ -3,7 +3,6 @@
 cabal sandbox delete
 cabal sandbox init
 
-cabal sandbox add-source $HOME/repositories/ansi-wl-pprint
 cabal sandbox add-source $HOME/work/dev/algebra-dag
 cabal install --dependencies-only
 cabal configure
