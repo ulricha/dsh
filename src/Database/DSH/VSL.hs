4a8a08b4c388dc1ae722f28964ef512a8755392d
Alexander Ulrich <alex@etc-network.de>
2016-06-05 12:24:31 +0200

    Export VSL language and class to backend packages.

--- src/Database/DSH/VSL.hs
+++ src/Database/DSH/VSL.hs
@@ -3,12 +3,16 @@
 
 module Database.DSH.VSL
     ( module Database.DSH.VSL.Lang
+    , module Database.DSH.VSL.VirtualSegmentAlgebra
+    , module Database.DSH.Common.VectorLang
     ) where
 
-import           Database.DSH.VSL.Lang            (VSL)
+import           Database.DSH.Common.VectorLang
+import           Database.DSH.Translate.Vectorize
+import           Database.DSH.VSL.Lang                  (VSL)
 import           Database.DSH.VSL.Opt.OptimizeVSL
 import           Database.DSH.VSL.Vectorize
-import           Database.DSH.Translate.Vectorize
+import           Database.DSH.VSL.VirtualSegmentAlgebra
 
 instance VectorLang VSL where
     vectorize = vectorizeDelayed
