883bb28ba0886a27b226fa12a99350f528b48254
Alexander Ulrich <alex@etc-network.de>
2016-06-06 12:32:22 +0200

    Generalize interface for algebra translation.
    Previously, translation from a vector language to an algebraic plan
    returned the vector language-specific monad. The relational backend
    would use this computation to insert Serialize operators. With this API
    however, it is not possible to re-use the Serialize code for multiple
    vector languages (i.e. relational implementation of SL and VSL).
    
    Here, we return the computation in the generic Build monad, stripping
    away the layer specific to vector languages.

--- src/Database/DSH/Translate/SL2Algebra.hs
+++ src/Database/DSH/Translate/SL2Algebra.hs
@@ -13,7 +13,6 @@ import qualified Data.Traversable               as T
 
 import           Control.Monad.State
 
-import qualified Database.Algebra.Dag           as D
 import qualified Database.Algebra.Dag.Build     as B
 import           Database.Algebra.Dag.Common
 
@@ -33,8 +32,8 @@ type VecBuild a d r k f s = StateT (Cache d r k f s) (B.Build a)
 
 runVecBuild :: SegmentAlgebra a
             => VecBuild a (SLDVec a) (SLRVec a) (SLKVec a) (SLFVec a) (SLSVec a) r
-            -> (D.AlgebraDag a, r, NodeMap [Tag])
-runVecBuild c = B.runBuild $ fst <$> runStateT c IM.empty
+            -> B.Build a r
+runVecBuild c = evalStateT c IM.empty
 
 data Res d r k f s
     = RRVec r
@@ -142,13 +141,9 @@ getSL n vlNodes = case IM.lookup n vlNodes of
 pp :: NodeMap SL.SL -> String
 pp m = intercalate ",\n" $ map show $ IM.toList m
 
-vl2Algebra :: SegmentAlgebra a
-           => NodeMap SL.SL
-           -> Shape V.DVec
-           -> VecBuild a (SLDVec a) (SLRVec a) (SLKVec a) (SLFVec a) (SLSVec a) (Shape (SLDVec a))
-vl2Algebra vlNodes plan = do
+vl2Algebra :: SegmentAlgebra a => NodeMap SL.SL -> Shape V.DVec -> B.Build a (Shape (SLDVec a))
+vl2Algebra vlNodes plan = runVecBuild $ do
     mapM_ (translate vlNodes) roots
-
     refreshShape plan
   where
     roots :: [AlgNode]
--- src/Database/DSH/VSL.hs
+++ src/Database/DSH/VSL.hs
@@ -5,6 +5,7 @@ module Database.DSH.VSL
     ( module Database.DSH.VSL.Lang
     , module Database.DSH.VSL.VirtualSegmentAlgebra
     , module Database.DSH.Common.VectorLang
+    , module Database.DSH.Translate.VSL2Algebra
     ) where
 
 import           Database.DSH.Common.VectorLang
@@ -13,6 +14,7 @@ import           Database.DSH.VSL.Lang                  (VSL, SegmentLookup(..))
 import           Database.DSH.VSL.Opt.OptimizeVSL
 import           Database.DSH.VSL.Vectorize
 import           Database.DSH.VSL.VirtualSegmentAlgebra
+import           Database.DSH.Translate.VSL2Algebra
 
 instance VectorLang VSL where
     vectorize = vectorizeDelayed
