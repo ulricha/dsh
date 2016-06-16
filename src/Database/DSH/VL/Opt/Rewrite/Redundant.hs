b6cb15fa14f3d8b451b6f010e3dcb3ad5748be7c
Alexander Ulrich <alex@etc-network.de>
2016-06-16 15:10:48 +0200

    Pull number operators through join-like operators.

--- src/Database/DSH/VL/Opt/Rewrite/Redundant.hs
+++ src/Database/DSH/VL/Opt/Rewrite/Redundant.hs
@@ -46,6 +46,7 @@ redundantRules = [ pullProjectAppKey
                  , pushAggrSegReplicateScalar
                  , pushUnboxSngAlign
                  , pushUnboxSngReplicateScalar
+                 , pullNumberReplicateNest
                  ]
 
 
@@ -103,6 +104,8 @@ redundantRulesBottomUp = [ sameInputAlign
                          , pullProjectReplicateScalarRight
                          , selectCartProd
                          , pushUnboxSngThetaJoinRight
+                         , pullNumberAlignLeft
+                         , pullNumberAlignRight
                          ]
 
 redundantRulesAllProps :: VLRuleSet Properties
@@ -1408,3 +1411,46 @@ pushUnboxSngReplicateScalar q =
            r1Node    <- insert $ UnOp R1 unboxNode
            distNode  <- insert $ BinOp ReplicateScalar $(v "q1") r1Node
            void $ replaceWithNew q $ UnOp R1 distNode |])
+
+--------------------------------------------------------------------------------
+
+pullNumberReplicateNest :: VLRule ()
+pullNumberReplicateNest q =
+  $(dagPatMatch 'q "R1 ((q1) ReplicateNest (Number (q2)))"
+    [| return $ do
+          logRewrite "Redundant.ReplicateNest.Number" q
+          repNode <- insert $ BinOp ReplicateNest $(v "q1") $(v "q2")
+          r1Node  <- insert $ UnOp R1 repNode
+          void $ replaceWithNew q $ UnOp Number r1Node
+     |])
+
+pullNumberAlignLeft :: VLRule BottomUpProps
+pullNumberAlignLeft q =
+  $(dagPatMatch 'q "(Number (q1)) Align (q2)"
+     [| do
+          w1 <- vectorWidth . vectorTypeProp <$> properties $(v "q1")
+          w2 <- vectorWidth . vectorTypeProp <$> properties $(v "q2")
+          return $ do
+            logRewrite "Redundant.Align.Number.Left" q
+            -- Project the number output between left and right columns to
+            -- preserve the schema.
+            let proj = map Column ([1..w1] ++ [w1 + w2 + 1] ++ [w1+1..w1+w2])
+            trace (show proj) $ return ()
+            alignNode  <- insert $ BinOp Align $(v "q1") $(v "q2")
+            numberNode <- insert $ UnOp Number alignNode
+            void $ replaceWithNew q $ UnOp (Project proj) numberNode
+      |]
+   )
+
+pullNumberAlignRight :: VLRule BottomUpProps
+pullNumberAlignRight q =
+  $(dagPatMatch 'q "(q1) Align (Number (q2))"
+     [| do
+          return $ do
+            logRewrite "Redundant.Align.Number.Right" q
+            -- Project the number output between left and right columns to
+            -- preserve the schema.
+            alignNode  <- insert $ BinOp Align $(v "q1") $(v "q2")
+            void $ replaceWithNew q $ UnOp Number alignNode
+      |]
+   )
