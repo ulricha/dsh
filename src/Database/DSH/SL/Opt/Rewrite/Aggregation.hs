f7a9803831fb9387425f173d4803633a871422d6
Alexander Ulrich <alex@etc-network.de>
2017-03-06 07:44:00 +0100

    Remove SL/VSL group/fold fusion rewrites.

--- src/Database/DSH/SL/Opt/Rewrite/Aggregation.hs
+++ src/Database/DSH/SL/Opt/Rewrite/Aggregation.hs
@@ -9,7 +9,6 @@ import           Control.Monad
 
 import           Database.Algebra.Dag.Common
 
-import           Database.DSH.Common.Nat
 import           Database.DSH.Common.Opt
 import           Database.DSH.Common.VectorLang
 import           Database.DSH.SL.Lang
@@ -17,20 +16,10 @@ import           Database.DSH.SL.Opt.Properties.Types
 import           Database.DSH.SL.Opt.Rewrite.Common
 
 aggregationRules :: SLRuleSet TExpr TExpr ()
-aggregationRules = [ inlineFoldProject
-                   , flatGrouping
-                   -- , mergeGroupAggrFold
-                   -- , mergeNonEmptyAggrs
-                   -- , mergeGroupAggr
-                   , mergeGroupWithGroupAggrLeft
-                   , mergeGroupWithGroupAggrRight
-                   ]
+aggregationRules = [ inlineFoldProject ]
 
 aggregationRulesBottomUp :: SLRuleSet TExpr TExpr BottomUpProps
-aggregationRulesBottomUp = [ {- nonEmptyAggr
-                           , nonEmptyAggrSeg -}
-                             countDistinct
-                           ]
+aggregationRulesBottomUp = [ ]
 
 groupingToAggregation :: SLRewrite TExpr TExpr Bool
 groupingToAggregation =
@@ -38,48 +27,6 @@ groupingToAggregation =
     $ sequenceRewrites [ applyToAll inferBottomUp aggregationRulesBottomUp
                        , applyToAll noProps aggregationRules
                        ]
-
--- -- FIXME this rewrite will no longer work: take the UnboxSngS
--- -- operator into account.
--- mergeNonEmptyAggrs :: SLRule TExpr TExpr ()
--- mergeNonEmptyAggrs q =
---   $(dagPatMatch 'q "(AggrNonEmptyS afuns1 (qi1)) Zip (AggrNonEmptyS afuns2 (qi2))"
---     [| do
---         predicate $ $(v "qi1") == $(v "qi2")
-
---         return $ do
---             logRewrite "Aggregation.NonEmpty.Merge" q
---             let afuns  = $(v "afuns1") <> $(v "afuns2")
---             let aggrOp = UnOp (AggrNonEmptyS afuns) $(v "qi1")
---             void $ replaceWithNew q aggrOp |])
-
--- -- | If we can infer that the vector is not empty, we can employ a
--- -- simplified version of the aggregate operator that does not add a
--- -- default value for an empty input.
--- nonEmptyAggr :: SLRule TExpr TExpr BottomUpProps
--- nonEmptyAggr q =
---   $(dagPatMatch 'q "Aggr aggrFun (q1)"
---     [| do
---         VProp True <- nonEmptyProp <$> properties $(v "q1")
-
---         return $ do
---             logRewrite "Aggregation.NonEmpty.Aggr" q
---             let aggrOp = UnOp (AggrNonEmpty ($(v "aggrFun") N.:| [])) $(v "q1")
---             void $ replaceWithNew q aggrOp |])
-
--- -- | If we can infer that all segments (if there are any) are not
--- -- empty, we can employ a simplified version of the aggregate operator
--- -- that does not add default values for empty segments.
--- nonEmptyAggrSeg :: SLRule TExpr TExpr BottomUpProps
--- nonEmptyAggrSeg q =
---   $(dagPatMatch 'q "(_) AggrSeg aggrFun (q2)"
---     [| do
---         VProp True <- nonEmptyProp <$> properties $(v "q2")
-
---         return $ do
---             logRewrite "Aggregation.NonEmpty.AggrSeg" q
---             let aggrOp = UnOp (AggrNonEmptyS ($(v "aggrFun") N.:| [])) $(v "q2")
---             void $ replaceWithNew q aggrOp |])
 
 -- | Merge a projection into a segmented aggregate operator.
 inlineFoldProject :: SLRule TExpr TExpr ()
@@ -92,156 +39,3 @@ inlineFoldProject q =
             logRewrite "Aggregation.Normalize.Fold.Project" q
             void $ replaceWithNew q $ UnOp (Fold afun') $(v "qi") |])
 
--- | We rewrite a combination of GroupS and aggregation operators into a single
--- GroupAggr operator.
-flatGrouping :: SLRule TExpr TExpr ()
-flatGrouping q =
-  $(dagPatMatch 'q "R1 (qu=(qr1=R1 (qg)) UnboxSng (Fold afun (R2 (qg1=Group groupExpr (q1)))))"
-    [| do
-
-        -- Ensure that the aggregate results are unboxed using the
-        -- outer vector of the grouping operator.
-        predicate $ $(v "qg") == $(v "qg1")
-
-        return $ do
-          logRewrite "Aggregation.Grouping.Aggr" q
-          void $ replaceWithNew q $ UnOp (GroupAggr ($(v "groupExpr"), pure $(v "afun"))) $(v "q1")
-        |])
-
--- | Cleanup rewrite: merge a segment aggregate with a group
--- operator. In contrast to rewrite 'flatGrouping", unboxing is performed using
--- a GroupAggr operator that has the same grouping expressions as the original
--- grouping. As both Group and GroupAggr use the grouping expressions on the
--- same input, we add the aggregate to the list of GroupAggr aggregates and use
--- only the GroupAggr operator.
---
--- This pattern can occur if R1 outputs of Group are moved to a
--- GroupAggr and segment aggregate and unboxing operators are pushed
--- down through segment propagation operators.
---
--- Testcase: TPC-H Q11, Q15
-
--- FIXME re-enable once type for groupaggr with multiple aggregates has been determined.
-
--- mergeGroupAggrFold :: SLRule TExpr TExpr ()
--- mergeGroupAggrFold q =
---   $(dagPatMatch 'q "R1 (qu=(qg=GroupAggr args (q1)) UnboxSng ((_) Fold afun (R2 (qg1=Group groupExprs (q2)))))"
---     [| do
---         predicate $ $(v "q1") == $(v "q2")
---         let (groupExprs', afuns) = $(v "args")
---         predicate $ groupExprs' == $(v "groupExprs")
-
---         return $ do
---           logRewrite "Aggregation.Grouping.Aggr2" q
-
---           let afunsCombined = afuns <> (pure $(v "afun"))
---           groupAggrNode <- insert $ UnOp (GroupAggr ($(v "groupExprs"), afunsCombined)) $(v "q1")
---           replace q groupAggrNode
-
---           -- Take care not to have duplicates of the grouping operator: Re-Wire
---           -- all parents of the original GroupAggr operator to the new
---           -- (extended) one and use a projection to keep the original schema.
---           gaParents <- filter (/= $(v "qu")) <$> parents $(v "qg")
---           let proj = map Column [1..(length groupExprs' + length afuns)]
---           projNode <- insert $ UnOp (Project proj) groupAggrNode
---           forM_ gaParents $ \p -> replaceChild p $(v "qg") projNode
---     |])
-
--- mergeGroupAggr :: SLRule TExpr TExpr ()
--- mergeGroupAggr q =
---   $(dagPatMatch 'q "(GroupAggr args1 (q1)) Align (GroupAggr args2 (q2))"
---     [| do
---         let (ges1, afuns1) = $(v "args1")
---         let (ges2, afuns2) = $(v "args2")
-
---         -- The rewrite can be applied if the same input is grouped
---         -- according to the same grouping expressions.
---         predicate $ ges1 == ges2
---         predicate $ $(v "q1") == $(v "q2")
-
---         return $ do
---           logRewrite "Aggregation.Normalize.MergeGroupAggr" q
---           groupNode <- insert $ UnOp (GroupAggr ($(v "ges1"), ($(v "afuns1") <> $(v "afuns2")))) $(v "q1")
-
---           -- Reconstruct the schema produced by Zip. Note that this
---           -- duplicates the grouping columns.
---           let groupWidth = length $(v "ges1")
---               aggrWidth1 = N.length afuns1
---               aggrWidth2 = N.length afuns2
---               groupCols  = [ Column c | c <- [1 .. groupWidth]]
-
---           let proj = groupCols
---                      ++
---                      [ Column $ c + groupWidth | c <- [1 .. aggrWidth1] ]
---                      ++
---                      groupCols
---                      ++
---                      [ Column $ c + groupWidth + aggrWidth1 | c <- [1 .. aggrWidth2] ]
-
---           void $ replaceWithNew q $ UnOp (Project proj) groupNode |])
-
--- | This is a cleanup rewrite: It applies in a situation when
--- aggregates have already been merged with Group into GroupAggr. If
--- the GroupAggr output is combined with the R1 output of Group on the
--- same input and grouping expressions via Align, the effect is that
--- only the grouping expressions are duplicated.
-mergeGroupWithGroupAggrLeft :: SLRule TExpr TExpr ()
-mergeGroupWithGroupAggrLeft q =
-  $(dagPatMatch 'q "(R1 (Group ges (q1))) Align (GroupAggr args (q2))"
-    [| do
-        let (ges', afuns) = $(v "args")
-
-        -- Input vectors and grouping expressions have to be the same.
-        predicate $ $(v "q1") == $(v "q2")
-        predicate $ $(v "ges") == ges'
-
-        return $ do
-            logRewrite "Aggregation.Normalize.MergeGroup.Left" q
-
-            -- To keep the schema, we have to duplicate the grouping
-            -- columns.
-            let e = TMkTuple [ TTupElem First TInput
-                             , TMkTuple [ TTupElem First TInput
-                                        , TTupElem (Next First) TInput
-                                        ]
-                             ]
-
-            groupNode <- insert $ UnOp (GroupAggr (ges', afuns)) $(v "q1")
-            void $ replaceWithNew q $ UnOp (Project e) groupNode |])
-
--- | The mirrored dual of rewrite
--- 'Aggregation.Normalize.MergeGroup.Left'.
-mergeGroupWithGroupAggrRight :: SLRule TExpr TExpr ()
-mergeGroupWithGroupAggrRight q =
-  $(dagPatMatch 'q "(GroupAggr args (q1)) Align (R1 (Group ges (q2)))"
-    [| do
-        let (ges', afuns) = $(v "args")
-
-        -- Input vectors and grouping expressions have to be the same.
-        predicate $ $(v "q1") == $(v "q2")
-        predicate $ $(v "ges") == ges'
-
-        return $ do
-            logRewrite "Aggregation.Normalize.MergeGroup.Right" q
-
-            -- To keep the schema, we have to duplicate the grouping
-            -- columns.
-            let e = TMkTuple [ TMkTuple [ TTupElem First TInput
-                                        , TTupElem (Next First) TInput
-                                        ]
-                             , TTupElem First TInput
-                             ]
-
-            groupNode <- insert $ UnOp (GroupAggr (ges', afuns)) $(v "q1")
-            void $ replaceWithNew q $ UnOp (Project e) groupNode |])
-
-countDistinct :: SLRule TExpr TExpr BottomUpProps
-countDistinct q =
-  $(dagPatMatch 'q "Fold a (Unique (q2))"
-    [| do
-        AggrCount           <- return $(v "a")
-
-        return $ do
-            logRewrite "CountDistinct" q
-            void $ replaceWithNew q $ UnOp (Fold (AggrCountDistinct TInput)) $(v "q2")
-        |])
