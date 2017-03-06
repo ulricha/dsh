2cefe8721ee91257e9469dc7247a2df10cfe7b79
Alexander Ulrich <alex@etc-network.de>
2017-03-06 16:46:26 +0100

    Merge stacked groupjoin operators.

--- src/Database/DSH/CL/Opt/GroupJoin.hs
+++ src/Database/DSH/CL/Opt/GroupJoin.hs
@@ -18,17 +18,20 @@ module Database.DSH.CL.Opt.GroupJoin
 import           Control.Arrow
 
 import           Data.List
-import           Data.List.NonEmpty             (NonEmpty ((:|)))
-import qualified Data.Map                       as M
-import qualified Data.Set                       as S
+import           Data.List.NonEmpty            (NonEmpty ((:|)))
+import qualified Data.List.NonEmpty            as N
+import qualified Data.Map                      as M
+import           Data.Semigroup                ((<>))
+import qualified Data.Set                      as S
 
 import           Database.DSH.Common.Kure
 import           Database.DSH.Common.Lang
+import           Database.DSH.Common.Nat
 
 import           Database.DSH.CL.Kure
 import           Database.DSH.CL.Lang
 
-import qualified Database.DSH.CL.Primitives     as P
+import qualified Database.DSH.CL.Primitives    as P
 
 import           Database.DSH.CL.Opt.Auxiliary
 import           Database.DSH.CL.Opt.FoldGroup
@@ -193,7 +196,7 @@ disjunctiveGroupJoinR = logR "groupjoin.disjunctive" $ do
 --------------------------------------------------------------------------------
 
 optimizeGroupJoinR :: RewriteC CL
-optimizeGroupJoinR = disjunctiveGroupJoinR
+optimizeGroupJoinR = disjunctiveGroupJoinR <+ mergeGroupjoinR
 
 --------------------------------------------------------------------------------
 -- Initial Fold/Group fusion for aggregates in qualifiers
@@ -290,3 +293,94 @@ mergegroupjoinHeadR = logR "groupjoin.construct.head" $ do
     Comp ty h _ <- promoteT idR
     (qs', h') <- childT CompQuals $ mergeGroupJoinSpineHeadT h
     pure $ inject $ Comp ty h' qs'
+
+--------------------------------------------------------------------------------
+-- Merging of GroupJoin operators
+
+-- | Check whether the join predicates of two stacked GroupJoins are compatible
+-- for merging. They are compatible if the predicate of the topmost join refers
+-- to the first tuple component of the left input and they are otherwise
+-- identical.
+leftCompatible :: ScalarExpr -> ScalarExpr -> Bool
+leftCompatible (JBinOp op e1 e2) (JBinOp op' e1' e2') =
+    op == op' && leftCompatible e1 e1' && leftCompatible e2 e2'
+leftCompatible (JUnOp op e) (JUnOp op' e') =
+    op == op' && leftCompatible e e'
+leftCompatible (JLit _ v) (JLit _ v') =
+    v == v'
+leftCompatible (JTupElem First JInput{}) (JInput _) = True
+leftCompatible (JTupElem n e) (JTupElem n' e') =
+    n == n' && leftCompatible e e'
+leftCompatible _ _ = False
+
+-- | Merge two group joins into one if their join predicates and left inputs are
+-- compatible.
+mergeGroupjoinR :: RewriteC CL
+mergeGroupjoinR = logR "groupjoin.merge" $ do
+    GroupJoinP _ p1 (NE (a1 :| [])) (GroupJoinP ty p2 (NE as) xs ys) ys' <- promoteT idR
+    guardM $ ys == ys'
+
+    guardM $ and $ N.zipWith (\c1 c2 -> leftCompatible (jcLeft c1) (jcLeft c2)
+                                        && jcOp c1 == jcOp c2
+                                        && jcRight c1 == jcRight c2)
+                             (jpConjuncts p1)
+                             (jpConjuncts p2)
+
+    mergeExistingAggrR a1 as (elemT ty) p2 xs ys <+ mergeNewAggrR a1 as p2 xs ys
+
+-- | If the aggregate from the upper GroupJoin is already computed in the lower
+-- GroupJoin, reuse that one.
+mergeExistingAggrR :: AggrApp -> N.NonEmpty AggrApp -> Type -> JoinPredicate ScalarExpr -> Expr -> Expr -> RewriteC CL
+mergeExistingAggrR a as ty p xs ys = do
+    -- The argument expression of aggregate 'a' refers to the lower
+    -- GroupJoin output tuples. We have to eliminate references to the first
+    -- tuple component and change the type of the input accordingly.
+    let xt  = elemT $ typeOf xs
+        yt  = elemT $ typeOf ys
+        a'  = a { aaArg = mapInput (const $ TupleT [xt, yt]) $ unFst (aaArg a) }
+
+    Just aggIndex <- return $ elemIndex a' $ N.toList as
+    let combinedJoin = GroupJoinP (ListT ty) p (NE as) xs ys
+
+    ga <- freshNameT []
+    let gav = Var ty ga
+
+    let h = P.pair (P.tuple $ map (\i -> P.tupElem (intIndex i) gav) [1..length as + 1])
+                   (P.tupElem (intIndex $ aggIndex + 2) gav)
+    return $ inject $ P.singleGenComp h ga combinedJoin
+
+-- | Change a scalar expression that only refers to the first tuple component of
+-- the input to refer directly to the input.
+unFst :: ScalarExpr -> ScalarExpr
+unFst (JIf e1 e2 e3)                             = JIf (unFst e1) (unFst e2) (unFst e3)
+unFst (JBinOp op e1 e2)                          = JBinOp op (unFst e1) (unFst e2)
+unFst (JUnOp op e)                               = JUnOp op (unFst e)
+unFst (JTupElem First (JInput (TupleT [t1, _]))) = JInput t1
+unFst (JTupElem i (JInput ty))                   = JTupElem i (JInput ty)
+unFst (JTupElem idx e)                           = JTupElem idx (unFst e)
+unFst (JLit ty val)                              = JLit ty val
+unFst (JInput _)                                 = error "unFst"
+
+-- | If the aggregate computed by the upper GroupJoin does not exist in the
+-- lower GroupJoin, add it there and eliminate the upper GroupJoin.
+mergeNewAggrR :: AggrApp -> N.NonEmpty AggrApp -> JoinPredicate ScalarExpr -> Expr -> Expr -> RewriteC CL
+mergeNewAggrR a as p xs ys = do
+    let xt  = elemT $ typeOf xs
+        yt  = elemT $ typeOf ys
+        a1t = aggType a
+        ats = map aggType $ N.toList as
+        gt  = TupleT $ xt : ats ++ [a1t]
+        -- The argument expression of aggregate 'a' refers to the lower
+        -- GroupJoin output tuples. We have to eliminate references to the first
+        -- tuple component and change the type of the input accordingly.
+        a'  = a { aaArg = mapInput (const $ TupleT [xt, yt]) $ unFst (aaArg a) }
+        as' = as <> pure a'
+        combinedJoin = GroupJoinP (ListT gt) p (NE as') xs ys
+
+    ga <- freshNameT []
+    let gav = Var gt ga
+
+    let h = P.pair (P.tuple $ map (\i -> P.tupElem (intIndex i) gav) [1..length as + 1])
+                   (P.tupElem (intIndex $ length as + 2) gav)
+    return $ inject $ P.singleGenComp h ga combinedJoin
+
